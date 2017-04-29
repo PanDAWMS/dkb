1c427655fe0a4e67abab6b52e0ea23e0fa6c89e7
Marina Golosova <golosova.marina@gmail.com>
2017-04-29 13:04:42 +0300

    Revert "Initial commit (migration from Subversion)."
    This reverts commit 10d09127959a2bf7e1798bfc4efbf7b3c84fbf12.
    
    Reason: failed to create an accurate snapshot of the current state.

--- Utils/Dataflow/000_kafka/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-[ -z "$KAFKA_HOME" ] && KAFKA_HOME=/usr/local/kafka/default
-
-for f in ./libs/*.jar $KAFKA_HOME/libs/*jar $KAFKA_HOME/dependant-libs/*/*.jar; do
-  CLASSPATH="$CLASSPATH:$f"
-done
-
-javac -cp $CLASSPATH $*
