74a465901c96843092042518f65f57147b4a6e4b
Igor Tkachenko <IATkachenko@gmail.com>
2017-06-15 12:22:06 +0300

    Added kibana installation actions
    Kibana is very useful if you want to understand what happens with data in elasticsearch.
    
    kibana_rpms function will install kibana rpms. Kibana needed java,
      but java is not installed in this part, because other components may require different
      java version than openjdk from repository.
    
    kibana_bin function will install kibana from precompiled binary. You should specify
      KIBANA_DIR to /usr/local/something if you want to use binary installation method.
    We keep kibana version same as elasticsearch version, because it is very important
      to have same version for elasticsearch and kibana.
    
    kibana_config will confugure kibana basing on installation method.
    It will create user 'kibana' and move service script to /etc/init.d, if used binary
      instalation method.
    It assumes that elasticsearch runs at same machine at port 9200.
    For security reason kibana will listen 127.0.0.1 address.
    If we need access to kibana from outside it is the best way to have nginx proxy
      with authorization.

--- Installation/Elasticsearch/install.sh
+++ Installation/Elasticsearch/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+KIBANA_MODE='rpms'
+#KIBANA_DIR='/usr/local/kibana'	#uncomment for binary installation
+[ -n "$KIBANA_DIR" ] && KIBANA_MODE='bin'
 
 #ES and kibana versions must be in sync.
 VERSION='5.4.1'
@@ -38,7 +41,47 @@ EOF
   yum install -y elasticsearch-$VERSION
 }
 
-LIST='es_rpms es_coinfig'
+kibana_rpms() {
+  cat <<EOF >/etc/yum.repos.d/kibana.repo
+[kibana-5.x]
+name=Kibana repository for 5.x packages
+baseurl=https://artifacts.elastic.co/packages/5.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+  yum install -y kibana-$VERSION
+}
+kibana_bin() {
+  BIN_FILE="kibana-${VERSION}-linux-x86_64.tar.gz"
+  wget https://artifacts.elastic.co/downloads/kibana/$BIN_FILE -O $BIN_FILE
+  mkdir -p "$KIBANA_DIR"
+  tar -xzf $BIN_FILE -C $KIBANA_DIR --strip-components=1
+  rm -f "$BIN_FILE"
+  grep -e '^kibana' /etc/passwd && useradd -r kibana
+  chown kibana:kibana "$KIBANA_DIR" -R
+}
+kibana_config() {
+  KIBANA_CONFIG='/etc/kibana/kibana.yml'
+  mkdir -p `dirname $KIBANA_CONFIG`
+  [ -f "$KIBANA_CONFIG.orig" ] && mv $KIBANA_CONFIG{,.orig}
+  cat <<EOF >$KIBANA_CONFIG
+server.port: 5601
+server.host: 127.0.0.1
+elasticsearch.url: http://127.0.0.1:9200
+server.name: `hostname -s`
+EOF
+  [ "$KIBANA_MODE" == 'bin' ] && \
+    cp kibana.init.d /etc/init.d/kibana && \
+    sed -i -e "s|%%%KIBANA_DIR%%%|$KIBANA_DIR|" /etc/init.d/kibana && \
+    chkconfig --add kibana
+  chkconfig --levels 345 kibana on
+  service kibana start
+}
+
+LIST="es_rpms es_coinfig kibana_${KIBANA_MODE} kibana_config"
 [ -n "$1" ] && LIST="$*"
 for action in $LIST; do
   $action
