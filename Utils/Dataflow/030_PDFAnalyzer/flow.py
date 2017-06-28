b2da414702b7df27d07486a27df440d565d1bdff
Evildoor <evildoor256@gmail.com>
2017-06-28 15:48:29 +0300

    DF/30: Added pyDKB integration.
    flow.py contains process() function which allows PDFAnalyzer to be
    called as a pyDKB module. Removed obsolete kafkamode.py, its
    functions are now performed either by flow.py(PDF processing in non
    interactive mode) or pyDKB(input and output handling).
    It should be noticed that new workflow mode implemented in flow.py
    accepts JSON files instead of strings and passes dkbID from input
    to output (see previous stage output).

---
+++ Utils/Dataflow/030_PDFAnalyzer/flow.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import json
+import os
+import shutil
+import subprocess
+import sys
+
+from manager import cfg
+from manager import path_join
+from manager import Paper
+from manager import re_pdfname
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(base_dir))
+import pyDKB
+
+def process(stage, msg):
+    # Obtain the PDF name from the input. Input is expected to be a JSON containing a single dictionary with 2 items:
+    # PDF - name of the PDF.
+    # dkbID - must be passed forward.
+    inp = msg.content()
+    if "PDF" not in inp:
+        sys.stderr.write("Error: no PDF specified.\n")
+        return False
+    fname = inp["PDF"]
+
+    # Process the PDF and export it.
+    if fname.startswith("hdfs://"):
+        hdfs = True
+        fname = path_join(cfg["HDFS_PDF_DIR"], fname[len("hdfs://"):])
+    else:
+        hdfs = False
+        fname = os.path.abspath(fname).replace("\\", "/")
+        if not os.access(fname, os.F_OK):
+            sys.stderr.write("No such file or directory:" + fname + "\n")
+            return False
+    pdfname = re_pdfname.search(fname)
+    if not pdfname:
+        sys.stderr.write("File " + fname + " is not a pdf file.\n")
+        return False
+    else:
+        pdfname = pdfname.group(1)
+        dirname = path_join(cfg["WORK_DIR"], "%s_tmp" % (pdfname))
+        os.mkdir(dirname)
+        try:
+            if hdfs:
+                command_list = cfg["HDFS_DOWNLOAD_COMMAND"].split() + [fname, dirname]
+                subprocess.call(command_list, stderr = sys.stderr, stdout = sys.stderr)
+            else:
+                shutil.copy(fname, dirname)
+        except Exception as e:
+            sys.stderr.write("Failed to copy file into temporary directory\n")
+            if hdfs:
+                sys.stderr.write("hdfs download command:" + str(command_list)
+                                 + "\n")
+            sys.stderr.write(str(e) + "\n")
+            shutil.rmtree(dirname)
+            return False
+        p = Paper(pdfname, dirname)
+        outf = pdfname + ".json"
+        p.mine_text()
+        p.export(quick = True, outf = outf)
+        p.delete()
+        with open(outf, "r") as f:
+            outp = json.load(f)
+        os.remove(outf)
+
+    # Construct the output.
+    if "dkbID" in inp:
+        outp["dkbID"] = inp["dkbID"]
+    outMessage = pyDKB.dataflow.Message(pyDKB.dataflow.messageType.JSON)(outp)
+    stage.output(outMessage)
+    return True
+
+if __name__ == "__main__":
+    stage = pyDKB.dataflow.stage.JSONProcessorStage()
+    stage.process = process
+
+    try:
+        stage.parse_args(sys.argv[1:])
+        stage.run()
+    except (pyDKB.dataflow.DataflowException, RuntimeError), e:
+        if str(e):
+            sys.stderr.write("Error while running stage 30: %s\n" % e)
+    finally:
+        stage.stop()
+    
