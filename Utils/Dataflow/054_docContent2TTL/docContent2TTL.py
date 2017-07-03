ccb76310d8f5d728a15e7d85a70ef10e2e21f7ac
Anastasiia Kaida <anastasiakaida@gmail.com>
2017-07-04 02:31:57 +0700

    Adding try...except blocks
    Adding try...except blocks to prevent 'KeyError'.
    
    Also:
    Adding 25.json (missing dkbID).
    Each entry is assigned as a variable:
    *data_taking_year
    *luminosity
    *energy

--- Utils/Dataflow/054_docContent2TTL/docContent2TTL.py
+++ Utils/Dataflow/054_docContent2TTL/docContent2TTL.py
@@ -29,8 +29,13 @@ def process(stage, msg):
 
 def doc_content_triples(data):
 	dataset_suffix = "_datasets"
+	
+	try:
+		dkbID = data['dkbID']
+	except KeyError:
+		sys.stderr.write("No dkbID.\n")
+		return False
 
-	dkbID = data['dkbID']
 	triples = []
 	for i, item in enumerate(data['content']):
 		DEFAULT = {
@@ -53,34 +58,56 @@ def doc_content_triples(data):
 				#print('{document_ID}_{content_name}'.format(**DATASAMPLES))
 				triples.append('''<{graph}/datasample/{data_sample}> a <{ontology}#DataSample> .'''.format(**DATASAMPLES))
 				triples.append('''<{graph}/document/{document_ID}/{content_name}> <{ontology}#mentionsDataSample> <{graph}/datasample/{data_sample}> .'''.format(**DATASAMPLES))
-		if item == 'plain_text':	
+		if item == 'plain_text':
+			
+			try:
+				data_taking_year = data['content'][item]['data taking year']
+			except KeyError:
+				data_taking_year = None
+			
+			try:
+				luminosity = data['content'][item]['luminosity'].replace(' ','_')
+			except (KeyError, AttributeError):
+				luminosity = None
+			
+			try:
+				energy = data['content'][item]['energy'].lower().replace(' ','')
+			except (KeyError, AttributeError):
+				energy = None
+			
 			PLAINTEXT = {
-				'data_taking_year' : data['content'][item]['data taking year'],
-				'luminosity' : data['content'][item]['luminosity'].replace(' ','_'),
-				'energy' : data['content'][item]['energy'].lower().replace(' ','')
+				'data_taking_year' : data_taking_year,
+				'luminosity' : luminosity,
+				'energy' : energy
 			}
 			PLAINTEXT.update(DEFAULT)
 			triples.append('''<{graph}/document/{document_ID}/{content_name}> a <{ontology}#DocumentContent> .'''.format(**PLAINTEXT))
 			triples.append('''<{graph}/document/{document_ID}> <{ontology}#hasContent> <{graph}/document/{document_ID}/{content_name}> .'''.format(**PLAINTEXT))
+			
 			#data taking year
 			triples.append('''<{graph}/document/{document_ID}/{content_name}> <{ontology}#mentionsDataTakingYear> "{data_taking_year}" .'''.format(**PLAINTEXT))
+			
 			#luminosity
 			triples.append('''<{graph}/luminosity/{luminosity}> a <{ontology}#Luminosity> .'''.format(**PLAINTEXT))
 			triples.append('''<{graph}/document/{document_ID}/{content_name}> <{ontology}#mentionsLuminosity> <{graph}/luminosity/{luminosity}> .'''.format(**PLAINTEXT))
+			
+			#energy
 			triples.append('''<{graph}/document/{document_ID}/{content_name}> <{ontology}#mentionsEnergy> <{ontology}#{energy}> .'''.format(**PLAINTEXT))
+			
 			#checking
 			#print('{data_taking_year}_{content_name}_{luminosity}'.format(**PLAINTEXT))
-			campaign = data['content'][item]['campaigns']
-			#print len(campaign)
-			if len(campaign) > 0:
+			try:
+				campaign = data['content'][item]['campaigns']
+				#print len(campaign)
 				for j, pt_item in enumerate(campaign):
 					PLAINTEXT["campaign"] = pt_item
 					triples.append('''<{graph}/campaign/{campaign}> a <{ontology}#Campaign> .'''.format(**PLAINTEXT))
 					triples.append('''<{graph}/document/{document_ID}/{content_name}> <{ontology}#mentionsCampaign> <{graph}/campaign/{campaign}> .'''.format(**PLAINTEXT))
 					del PLAINTEXT["campaign"]
-			else:
-				print ("No campaigns in this file.")
-				
+			except KeyError:
+				campaign = []
+			if not campaign:
+				print("No campaigns in this file.\n")
 	#for i, item in enumerate(triples):
 			#file.write(item)
 	return triples
