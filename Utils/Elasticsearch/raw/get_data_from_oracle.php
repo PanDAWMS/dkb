b5c32d10d6ed550be0d138b71e3d4310d87aa44a
Igor Tkachenko <IATkachenko@gmail.com>
2017-07-10 14:27:05 +0200

    Get query from external file
    After creating new branch for queries we have own place for queries to
    oracle, so we should use this file/files instead of hardcoded one.

--- Utils/Elasticsearch/raw/get_data_from_oracle.php
+++ Utils/Elasticsearch/raw/get_data_from_oracle.php
@@ -1,15 +1,13 @@
 <?php
 fscanf(STDIN, "%s\n", $p);
 
-$LIMIT='and ROWNUM<5';
-$LIMIT='';
-$fields = "lower(t.CAMPAIGN) as campaing, lower(t.SUBCAMPAIGN) as sub_campaing, t.phys_group as phys_group, t.project as project, t.PR_ID as request, t.TASKID as task_id, t.status as status, t.taskname as taskname, LISTAGG(hashtag.hashtag, ',') within group (order by ht_t.taskid) as hashtag_list, t.TOTAL_EVENTS as events_total, t.TOTAL_REQ_EVENTS as events_requested, t.timestamp as t_stamp, substr(t.taskname, instrc(t.taskname,'.',1,3)+1,instrc(t.taskname,'.',1,4)-instrc(t.taskname,'.',1,3)-1) as step";
-$tables = 'T_PRODUCTION_TASK t, atlas_deft.t_ht_to_task ht_t, atlas_deft.t_hashtag hashtag';
-$mc16 = "and lower(t.campaign) = 'mc16' and lower(t.subcampaign) = 'mc16a'";
-$conditions = "t.taskid = ht_t.taskid and ht_t.ht_id = hashtag.ht_id $mc16 $LIMIT";
-$group = "lower(t.CAMPAIGN), lower(t.SUBCAMPAIGN), t.phys_group, t.project, t.status, t.taskid, t.TOTAL_EVENTS, t.TOTAL_REQ_EVENTS, t.timestamp, t.taskname, t.PR_ID,substr(t.taskname, instrc(t.taskname,'.',1,3)+1,instrc(t.taskname,'.',1,4)-instrc(t.taskname,'.',1,3)-1)";
-$query = "select $fields FROM $tables WHERE $conditions GROUP BY $group";
-#echo "$query\n";
+$query_file = __DIR__.'/../../OracleProdSys2/mc16_campaign_for_ES.sql';
+$replaces = array(
+  '/--.*\n/' => '',
+  '/\s+/' => ' ',
+  '/;\s*$/' => ''
+);
+$query = preg_replace(array_keys($replaces), array_values($replaces), file_get_contents($query_file));
 
 $conn = oci_connect('atlas_deft_r', $p, 'adcr_adg');
 if (!$conn) {
