de47ab7f6953ab2bd499e56f7c4c9bf8495efe63
Igor Tkachenko <IATkachenko@gmail.com>
2017-08-08 14:40:22 +0200

    Added information messages to stderr
    Because we are going to use this script for creating json output we are
    have to send all information messages to stdout.

--- Utils/Elasticsearch/raw/get_data_from_oracle.php
+++ Utils/Elasticsearch/raw/get_data_from_oracle.php
@@ -1,4 +1,5 @@
 <?php
+fwrite(STDERR, "Password:\n");
 fscanf(STDIN, "%s\n", $p);
 
 $query_file = __DIR__.'/../../OracleProdSys2/mc16_campaign_for_ES.sql';
@@ -8,7 +9,7 @@ $replaces = array(
   '/;\s*$/' => ''
 );
 $query = preg_replace(array_keys($replaces), array_values($replaces), file_get_contents($query_file));
-
+fwrite(STDERR, "Will send query:\n\n$query\n\n");
 $conn = oci_connect('atlas_deft_r', $p, 'adcr_adg');
 if (!$conn) {
     $e = oci_error();
@@ -16,10 +17,10 @@ if (!$conn) {
 }
 
 $stid = oci_parse($conn, $query);
-#echo "Parsed!\n";
+fwrite(STDERR, "Parsed!\n");
 
 oci_execute($stid);
-#echo "Execution done!\n";
+fwrite(STDERR, "Execution done!\n");
 
 while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
   echo json_encode($row)."\n";
