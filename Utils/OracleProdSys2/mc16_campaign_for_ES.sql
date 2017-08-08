fee897105a763ac83bd054d7bfc5c0aabb56e9f9
Maria <magsend@gmail.com>
2017-08-08 16:28:48 +0300

    ProdSys2 SQL Query modifications
    - Physics Category definition removed from SQL
    - Added JOIN with t_production_tag table to obtain parameters:
       ATLAS Geometry, Condition Tags, SW Release, etc.

--- Utils/OracleProdSys2/mc16_campaign_for_ES.sql
+++ Utils/OracleProdSys2/mc16_campaign_for_ES.sql
@@ -2,12 +2,12 @@
 
 -- 1. (mc16_tasks) get all tasks for mc16 campaign, including some REQUEST params and restricted by GOOD status
 -- 2. (task_hashtags) get lists of hashtags for all mc16 tasks, defining step_names by step_id
--- 3. (phys_categories) define phys_category for each task, according with regular expressions,
--- taken from https://github.com/PanDAWMS/panda-bigmon-atlas/blob/devel/atlas/prodtask/hashtag.py
--- 4. FINALLY:
+-- 3. FINALLY:
 --    JOIN with ATLAS_PANDA.jedi_datasets to obtain events numbers
 --    nevents - number of input events
 --    neventsused - number of processed events
+--    JOIN with ATLAS_DEFT.t_production_tag to obtain such parameters as:
+--    atlas geometry, conditions tags, software release
 -- restrict query by GOOD dataset statuses
 with mc16_tasks as (
     select
@@ -15,9 +15,9 @@ with mc16_tasks as (
       t.taskid,
       t.step_id,
       t.taskname,
-      TO_CHAR(t.timestamp, 'DD-MM-RR HH24:MI:SS') as timestamp,
-      NVL(TO_CHAR(t.start_time, 'DD-MM-RR HH24:MI:SS'),'NA') as start_time,
-      NVL(TO_CHAR(t.endtime, 'DD-MM-RR HH24:MI:SS'),'NA') as end_time,
+      TO_CHAR(t.timestamp, 'dd-mm-yyyy hh24:mi:ss') as task_timestamp,
+      NVL(TO_CHAR(t.start_time, 'dd-mm-yyyy hh24:mi:ss'), '') as start_time,
+      NVL(TO_CHAR(t.endtime, 'dd-mm-yyyy hh24:mi:ss'), '') as end_time,
       t.subcampaign,
       t.project,
       t.phys_group,
@@ -44,7 +44,7 @@ with mc16_tasks as (
         t.status,
         t.taskid,
         t.taskname,
-        t.timestamp,
+        t.task_timestamp,
         t.start_time,
         t.end_time,
         t.description,
@@ -73,73 +73,11 @@ with mc16_tasks as (
         t.status,
         t.taskid,
         t.taskname,
-        t.timestamp,
+        t.task_timestamp,
         t.start_time,
         t.end_time,
         t.description,
         t.energy_gev
-  ),
-  phys_categories as (
-    select
-      campaign,
-      subcampaign,
-      phys_group,
-      project,
-      pr_id,
-      step_name,
-      status,
-      taskid,
-      taskname,
-      timestamp,
-      start_time,
-      end_time,
-      description,
-      energy_gev,
-      hashtag_list,
-      CASE
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(charmonium|jpsi|bs|bd|bminus|bplus|charm|bottom|bottomonium|b0)')
-        THEN 'BPhysics'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(btagging)')
-        THEN 'BTag'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(diboson|zz|ww|wz|wwbb|wwll)')
-        THEN 'Diboson'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(drellyan|dy)')
-        THEN 'DrellYan'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(exotic|monojet|blackhole|technicolor|randallsundrum|wprime|zprime|magneticmonopole|extradimensions|warpeded|contactinteraction|seesaw)')
-        THEN 'Exotic'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(photon|diphoton)')
-        THEN 'GammaJets'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(vbf|higgs|mh125|zhiggs|whiggs|bsmhiggs|chargedhiggs|bsmhiggs|smhiggs)')
-        THEN 'Higgs'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(minbias)')
-        THEN 'Minbias'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(dijet|multijet|qcd)')
-        THEN 'Multijet'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(performance)')
-        THEN 'Performance'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(singleparticle)')
-        THEN 'SingleParticle'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(singletop)')
-        THEN 'SingleTop'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(bino|susy|pmssm|leptosusy|rpv|mssm)')
-        THEN 'SUSY'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(triboson|triplegaugecoupling|zzw|www)')
-        THEN 'Triboson'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(ttbar)')
-        THEN 'TTbar'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(ttw|ttz|ttv|ttvv|4top)')
-        THEN 'TTbarX'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(upgrad)')
-        THEN 'Upgrade'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(w)')
-        THEN 'Wjets'
-        WHEN REGEXP_LIKE(lower(hashtag_list), '(z)')
-        THEN 'Zjets'
-      ELSE 'Uncategorized'
-      END as phys_category
-    from task_hashtags
-    where
-      REGEXP_LIKE(lower(hashtag_list), '((mc16[a-z]?)|(mc16[a-z]?_cp)|(mc16[a-z]?_trig)|(mc16[a-z]?_hpc)|(mc16[a-z]?_pc)|(mc16[a-z]?campaign))')
   )
       SELECT
         t.campaign,
@@ -151,22 +89,26 @@ with mc16_tasks as (
         t.status,
         t.taskid,
         t.taskname,
-        t.timestamp,
+        t.task_timestamp,
         t.start_time,
         t.end_time,
         t.hashtag_list,
-        t.phys_category,
         t.description,
         t.energy_gev,
         jd.datasetname,
         jd.status as dataset_status,
         jd.nevents AS requested_events,
-        jd.neventsused AS processed_events
+        jd.neventsused AS processed_events,
+        tag.name as tag_name,
+        tag.trf_release,
+        tag.tag_parameters
       FROM
-        phys_categories t,
-        ATLAS_PANDA.jedi_datasets jd
+        task_hashtags t,
+        ATLAS_PANDA.jedi_datasets jd,
+        ATLAS_DEFT.t_production_tag tag
       WHERE
         t.taskid = jd.jeditaskid
         AND jd.masterid IS NULL
         AND jd.type IN ('input')
-        and jd.status in ('ready','done','finished');
+        and jd.status in ('ready','done','finished')
+        and tag.name = trim(regexp_substr(trim(regexp_substr(t.taskname, '[^.]+',1,5)), '[^_]*$'));
