0380e433d86caf882ad61168686bcc7d5f8dbf03
Igor Tkachenko <IATkachenko@gmail.com>
2017-08-10 16:25:21 +0300

    Mapping updated according to stored query
    Mapping updatrd for
    https://github.com/PanDAWMS/dkb/commit/6af746e46dfc67adc5f165d9445e1e83bb39e053

--- Utils/Elasticsearch/raw/load_mapping.sh
+++ Utils/Elasticsearch/raw/load_mapping.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -H "Content-Type: application/x-ndjson" -XPUT 'http://localhost:9200/_template/raw_current?pretty' --data-binary "@raw_.mapping"
+curl -H "Content-Type: application/x-ndjson" -XPUT 'http://localhost:9200/_template/mc16?pretty' --data-binary "@mc16.mapping"
