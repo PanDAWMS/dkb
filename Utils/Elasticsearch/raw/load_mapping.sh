5dbbe6482c3a5dbee6012dc399294f5275a5bc35
Igor Tkachenko <IATkachenko@gmail.com>
2017-08-25 12:14:51 +0300

    Load mapping dynamically
    With these changes we can load any mapping based on file name

--- Utils/Elasticsearch/raw/load_mapping.sh
+++ Utils/Elasticsearch/raw/load_mapping.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -H "Content-Type: application/x-ndjson" -XPUT 'http://localhost:9200/_template/mc16?pretty' --data-binary "@mc16.mapping"
+[ -z "$1" -o ! -f "${1}.mapping" ] && echo "First argument should be filename without '.mapping' extension of mapping file!" || \
+curl -H "Content-Type: application/x-ndjson" -XPUT "http://localhost:9200/_template/$1?pretty" --data-binary "@$1.mapping"
