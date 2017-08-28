1f4059892d185201fb6ca15759cc5d1b0b906147
Evildoor <evildoor256@gmail.com>
2017-08-28 11:09:29 +0300

    DF/30: Changed/added file/function/class comments.
    1)Added file comments.
    2)Unified all comments according to flow.py.
    3)Unified all comments to be either "Do ..." or "Script/Class/
    Function ...".
    4)Wrapped long lines.

--- Utils/Dataflow/030_PDFAnalyzer/pdfwork.py
+++ Utils/Dataflow/030_PDFAnalyzer/pdfwork.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Note: this is a copy of a tool from pdfminer. May be modified in the future.
+
+"""
+PDF Analyzer script for extracting text from PDF with PDF Miner
+"""
+
 import sys
 from tempfile import TemporaryFile
 
@@ -16,7 +20,10 @@ from pdfminer.layout import LAParams
 #from pdfminer.pdftypes import resolve1
 
 def remove_ligatures(text):
-    # Replaces ligatures (fancy symbols combining several letters) with letter combinations.
+    """ Replace ligatures with corresponding letter combinations.
+
+    Ligatures are fancy symbols combining several letters.
+    """
     text = text.replace("ﬃ", "ffi")
     text = text.replace("ﬁ", "fi")
     text = text.replace("ﬀ", "ff")
@@ -24,10 +31,16 @@ def remove_ligatures(text):
     return text
 
 def get_page_text(interpreter, page, tmp, rotation=0):
-    # Extract text from a page with given rotation. These variables are required and must be setup in parent function:
-    # interpreter - interpreter used by PDFMiner.
-    # page - page object.
-    # tmp - temporary file use to get processed text. Stack Overflow advices to create a new tmp file each time, but it must be specified when creating PDFMiner device object, so i'm not sure if that's possible.
+    """ Extract text from a page with given rotation.
+
+    These variables are required and must be setup in parent function:
+    interpreter - interpreter used by PDFMiner.
+    page - page object.
+    tmp - temporary file use to get processed text. Stack Overflow
+    advices to create a new tmp file each time, but it must be specified
+    when creating PDFMiner device object, so i'm not sure if that's
+    possible in this case.
+    """
     page.rotate = (page.rotate+rotation) % 360
     interpreter.process_page(page)
     tmp.seek(0)
@@ -39,10 +52,14 @@ def get_page_text(interpreter, page, tmp, rotation=0):
     tmp.truncate()
     return text
 
-def mine_text(infname, page_numbers=False, outtype="text", rotated_pages=[], folder=False):
-    # Mine text from a PDF files. Find rotated pages if txt, rotate pages according to respective variable if xml.
-#    inf = open(infname, "rb")
-    with open(infname, "rb") as inf:  # By using "with" we ensure that file gets closed if something goes wrong in this block.
+def mine_text(infname, page_numbers=False, outtype="text", rotated_pages=[],
+              folder=False):
+    """ Mine text from a PDF files.
+
+    Rotated pages are found if output is txt, otherwise pages are
+    rotated according to respective variable.
+    """
+    with open(infname, "rb") as inf:
 
         rsrcmngr = PDFResourceManager()
 
@@ -56,14 +73,17 @@ def mine_text(infname, page_numbers=False, outtype="text", rotated_pages=[], fol
 
         extension = False
         if outtype == "text":
-            device = TextConverter(rsrcmngr, tmp, codec='utf-8', laparams=laparams)
+            device = TextConverter(rsrcmngr, tmp, codec='utf-8',
+                                   laparams=laparams)
             extension = "txt"
             rotated_pages = []
         elif outtype == "xml":
-            device = XMLConverter(rsrcmngr, tmp, codec='utf-8', laparams=laparams)
+            device = XMLConverter(rsrcmngr, tmp, codec='utf-8',
+                                  laparams=laparams)
             extension = "xml"
         elif outtype == "html":
-            device = HTMLConverter(rsrcmngr, tmp, codec='utf-8', laparams=laparams)
+            device = HTMLConverter(rsrcmngr, tmp, codec='utf-8',
+                                   laparams=laparams)
         interpreter = PDFPageInterpreter(rsrcmngr, device)
 
         n = 1
@@ -87,7 +107,8 @@ def mine_text(infname, page_numbers=False, outtype="text", rotated_pages=[], fol
                                 single += 1
                             normal += 1
                     coef = float(single) / normal
-    #                print "Page %d: %d/%d = %f lines contain a single character" % (n, single, normal, coef)
+    #                print "Page %d: %d/%d = %f lines contain a single \
+    #                character" % (n, single, normal, coef)
                     if coef > 0.6:
                         rotated_pages.append(n)
     #                    print "Page seems to be rotated"
@@ -104,41 +125,7 @@ def mine_text(infname, page_numbers=False, outtype="text", rotated_pages=[], fol
     return [n - 1, rotated_pages]
 
 if __name__ == "__main__":
-    [pages, rotated_pages] = mine_text("C:/Work/papers_analysis/ATL-COM-PHYS-2014-1357.pdf", [201], "xml")
+    fname = "C:/Work/papers_analysis/ATL-COM-PHYS-2014-1357.pdf"
+    [pages, rotated_pages] = mine_text(fname, [201], "xml")
     for p in pages:
         print pages[p]
-
-##    f = open("C:/Work/papers_analysis/ATL-COM-PHYS-2014-1430.pdf", "rb")
-##
-##    rsrcmngr = PDFResourceManager()
-##
-##    tmp = TemporaryFile(mode="w+")
-##    laparams = LAParams()
-##    device = TextConverter(rsrcmngr, tmp, codec='utf-8', laparams = laparams)
-##    interpreter = PDFPageInterpreter(rsrcmngr, device)
-##
-##    n = 1
-##    for page in PDFPage.get_pages(f):
-##        lines = get_page_lines(interpreter, page, tmp)
-##        single = 0
-##        normal = 0
-##        for l in lines:
-##            if not l.isspace():
-##                l = l.strip()
-##                if len(l) == 1:
-##                    single += 1
-##                normal += 1
-##        coef = float(single) / normal
-##        print "Page %d: %d/%d = %f lines contain a single character" % (n, single, normal, coef)
-##        if coef > 0.9:
-##            print "Page seems to be rotated"
-##            lines = get_page_lines(interpreter, page, tmp, 90)
-##            print lines
-##
-##        n += 1
-##        if n == 10:
-##            break
-##    print n
-##    f.close()
-##    device.close()
-##    tmp.close()
