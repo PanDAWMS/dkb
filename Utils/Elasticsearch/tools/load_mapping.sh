499e8826c0bbb7543020779385de69f5965c2c04
Igor Tkachenko <IATkachenko@gmail.com>
2017-09-01 19:53:04 +0300

    Get elasticsearch url from command line argument

--- Utils/Elasticsearch/tools/load_mapping.sh
+++ Utils/Elasticsearch/tools/load_mapping.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-curl -H "Content-Type: application/x-ndjson" -XPUT "http://localhost:9200/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
+usage() {
+  cat >&2 <<EOF
+  Usage: $0 [-e <ElasticSearch url>] mapping_file
+    -e -- url where we may found elasticsearch. localhost:9200 is default
+EOF
+  exit 1
+}
+
+ES='localhost'
+while getopts e: opt; do
+  case $opt in		
+    e) ES="$OPTARG";;
+    *) usage;
+  esac
+done
+shift $((OPTIND-1)) 
+
+curl -H "Content-Type: application/x-ndjson" -XPUT "http://${ES}:9200/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
