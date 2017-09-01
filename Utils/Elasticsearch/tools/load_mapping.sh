f2e2b2bd3c4b78848b53eff0089de415e4b213e0
Igor Tkachenko <IATkachenko@gmail.com>
2017-09-01 19:16:24 +0300

    Use file basename as template name

--- Utils/Elasticsearch/tools/load_mapping.sh
+++ Utils/Elasticsearch/tools/load_mapping.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 [ -z "$1" -o ! -f "${1}.mapping" ] && echo "First argument should be filename without '.mapping' extension of mapping file!" || \
-curl -H "Content-Type: application/x-ndjson" -XPUT "http://localhost:9200/_template/$1?pretty" --data-binary "@$1.mapping"
+curl -H "Content-Type: application/x-ndjson" -XPUT "http://localhost:9200/_template/`basename $1`?pretty" --data-binary "@$1.mapping"
