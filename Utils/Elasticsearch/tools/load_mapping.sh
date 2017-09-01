3192490eb0789a31678156ad5ab87e84510f1b1b
Igor Tkachenko <IATkachenko@gmail.com>
2017-09-01 20:03:32 +0300

    Added support of elasticeach config file, same as for process.sh

--- Utils/Elasticsearch/tools/load_mapping.sh
+++ Utils/Elasticsearch/tools/load_mapping.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 usage() {
   cat >&2 <<EOF
-  Usage: $0 [-e <ElasticSearch url>] [-a path/to/auth/file] mapping_file
-    -e -- url where we may found elasticsearch. localhost:9200 is default
+  Usage: $0 [-u <ElasticSearch url>] [-p <port>] [-a path/to/auth/file] [-c path/to/es/config] mapping_file
+    -u -- url where we may found elasticsearch. localhost is default
+    -p -- port where we may found elasticseatch at $ES_HOST
     -a -- auth information file for elasticsearch. File shoudl contain one string in following format:
       login:password
+    -c -- elasticsearch config file path. THis file may contain ES_HOST, ES_PORT, ES_USER and ES_PASSWORD variables
 EOF
   exit 1
 }
@@ -13,15 +15,21 @@ getAuthFromFile() {
   [ -f "$1" ] && cat "$1" || echo "File $1 not found. Auth will not be used." >&2 
 }
 
-ES='localhost'
-while getopts e:a: opt; do
+ES_HOST='localhost'
+ES_PORT=9200
+while getopts u:p::a:c: opt; do
   case $opt in		
-    e) ES="$OPTARG";;
+    u) ES_HOST="$OPTARG";;
+    p) ES_PORT="$OPTARG";;
     a) AUTH="$(getAuthFromFile $OPTARG)";;
+    c) 
+      [ -f "$OPTARG" ] && source "$OPTARG" || echo "Could not find file $OPTARG. Config not loaded!" >&2
+      [ -n "$ES_USER" -a -n "$ES_PASSWORD" ] && AUTH="$ES_USER:$ES_PASSWORD"
+    ;;
     *) usage;
   esac
 done
 [ -n "$AUTH" ] && AUTH="-u $AUTH" 
 shift $((OPTIND-1)) 
 
-curl $AUTH -H "Content-Type: application/x-ndjson" -XPUT "http://${ES}:9200/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
+curl $AUTH -H "Content-Type: application/x-ndjson" -XPUT "http://${ES_HOST}:${ES_PORT}/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
