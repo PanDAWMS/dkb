c8b40b224d1d870fcf25fb7a1a18ff541b7937a0
Igor Tkachenko <IATkachenko@gmail.com>
2017-09-01 19:53:43 +0300

    Get auth information for elasticsarch from file

--- Utils/Elasticsearch/tools/load_mapping.sh
+++ Utils/Elasticsearch/tools/load_mapping.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 usage() {
   cat >&2 <<EOF
-  Usage: $0 [-e <ElasticSearch url>] mapping_file
+  Usage: $0 [-e <ElasticSearch url>] [-a path/to/auth/file] mapping_file
     -e -- url where we may found elasticsearch. localhost:9200 is default
+    -a -- auth information file for elasticsearch. File shoudl contain one string in following format:
+      login:password
 EOF
   exit 1
 }
 
+getAuthFromFile() {
+  [ -f "$1" ] && cat "$1" || echo "File $1 not found. Auth will not be used." >&2 
+}
+
 ES='localhost'
-while getopts e: opt; do
+while getopts e:a: opt; do
   case $opt in		
     e) ES="$OPTARG";;
+    a) AUTH="$(getAuthFromFile $OPTARG)";;
     *) usage;
   esac
 done
+[ -n "$AUTH" ] && AUTH="-u $AUTH" 
 shift $((OPTIND-1)) 
 
-curl -H "Content-Type: application/x-ndjson" -XPUT "http://${ES}:9200/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
+curl $AUTH -H "Content-Type: application/x-ndjson" -XPUT "http://${ES}:9200/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
