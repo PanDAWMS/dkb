2ebbf7de306d645c4ea2ddb080d39b443879b916
Igor Tkachenko <IATkachenko@gmail.com>
2017-09-01 19:25:20 +0300

    Don't use file extension for template name

--- Utils/Elasticsearch/tools/load_mapping.sh
+++ Utils/Elasticsearch/tools/load_mapping.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ -z "$1" -o ! -f "${1}.mapping" ] && echo "First argument should be filename without '.mapping' extension of mapping file!" || \
-curl -H "Content-Type: application/x-ndjson" -XPUT "http://localhost:9200/_template/`basename $1`?pretty" --data-binary "@$1.mapping"
+[ -z "$1" -o ! -f "${1}" ] && echo "First argument should be path to  mapping file!" || \
+curl -H "Content-Type: application/x-ndjson" -XPUT "http://localhost:9200/_template/`basename ${1%.*}`?pretty" --data-binary "@${1}"
