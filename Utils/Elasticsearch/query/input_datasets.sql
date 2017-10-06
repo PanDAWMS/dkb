59b66ffa58aaa1eaf9f95ba49cff17a1e02cb10a
Maria <magsend@gmail.com>
2017-10-06 14:57:14 +0300

    Ora2ES, ProdSys Query: select input datasets for tasks

---
+++ Utils/Elasticsearch/query/input_datasets.sql
@@ -0,0 +1,15 @@
+-- select input datasets for all tasks
+-- for time period
+-- P.S. mininum value of t_production_task.timestamp is 2014-03-04 14:53:51.33390
+SELECT
+  t.taskid,
+  jd.datasetname as input_datasets
+FROM
+  t_production_task t
+  LEFT JOIN
+  ATLAS_PANDA.jedi_datasets jd
+  ON jd.jeditaskid = t.taskid
+WHERE
+  jd.type IN ('input')
+  AND t.timestamp > to_date('01-01-2016 00:00:00', 'dd-mm-yyyy hh24:mi:ss')
+  AND t.timestamp <= to_date('01-02-2016 01:00:00', 'dd-mm-yyyy hh24:mi:ss');
