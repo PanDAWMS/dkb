e7733488578f57de10754911207d78260e3e2144
Marina Golosova <golosova.marina@gmail.com>
2017-10-09 13:48:17 +0000

    DF/kafka: improve log file naming
    More accurate variable passing to the original Kafka run scripts
    to guarantee that the Streams application/connector log goes to
    the file with the appropriate name and that the file is handled
    by the Log4j (instead of using the console output and streaming
    it to the file).

--- Utils/Dataflow/000_kafka/runStreamsApplication.sh
+++ Utils/Dataflow/000_kafka/runStreamsApplication.sh
@@ -2,6 +2,6 @@
 
 base_dir=`dirname $0`
 
-export LOG4J_OPTS="-Dlog.file.name=StreamsApplication"
 
-$base_dir/run-class.sh ru.kiae.dkb.kafka.streams.cli.StreamsApplication "$@"
+
+$base_dir/run-class.sh $EXTRA_ARGS ru.kiae.dkb.kafka.streams.cli.StreamsApplication "$@"
