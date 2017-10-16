5f91c4ae21f6fa7ad579f97367bf40438130d915
Marina Golosova <golosova.marina@gmail.com>
2017-10-16 11:43:46 +0000

    DF/69: use ES config file (provides host, port, auth)
    The functionality was implemented in Elasticsearch/process.sh,
      so just make the code reusable.

--- Utils/Dataflow/069_upload2es/load_data.sh
+++ Utils/Dataflow/069_upload2es/load_data.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-curl 'http://127.0.0.1:9200/_bulk?pretty' --data-binary @${1}
+base_dir=$( cd "$( dirname "$( readlink -f "$0" )" )" && pwd )
+
+ES_CONFIG="$base_dir/../../Elasticsearch/config/es"
+
+ES_HOST='127.0.0.1'
+ES_PORT='9200'
+[ -f "$ES_CONFIG" ] && source "$ES_CONFIG"
+[ -n "$ES_USER" -a "$ES_PASSWORD" ] && ES_AUTH="--user ${ES_USER}:${ES_PASSWORD}"
+
+curl $ES_AUTH "http://$ES_HOST:$ES_PORT/_bulk?pretty" --data-binary @${1}
--- Utils/Elasticsearch/process.sh
+++ Utils/Elasticsearch/process.sh
@@ -30,16 +30,8 @@ convertDataToESFormat() {
 putDataToES() {
   #step 3 - load data to ES
 
-  #some default settings and configs loading
-  ES_CONFIG="$base_dir/config/es"
-  log "Loading defaults and config $ES_CONFIG if any"
-  ES_HOST='127.0.0.1'
-  ES_PORT='9200'
-  [ -f "$ES_CONFIG" ] && source "$ES_CONFIG"
-  [ -n "$ES_USER" -a "$ES_PASSWORD" ] && ES_AUTH="--user ${ES_USER}:${ES_PASSWORD}"
-
   log "Putting data to ES"
-  curl $ES_AUTH "http://$ES_HOST:$ES_PORT/_bulk?pretty" --data-binary @- || exit 3
+  $base_dir/tools/load_data.sh - || exit 3
 }
 
 getDataFromOracle > $ORACLE_OUT 
