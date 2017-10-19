77bc9756994e1da4e43682f586efc6412f33949a
Marina Golosova <golosova.marina@gmail.com>
2017-10-19 13:09:37 +0000

    DF/19: add checks for empty fields.

--- Utils/Dataflow/019_oracle2esFormat/oracle2es.php
+++ Utils/Dataflow/019_oracle2esFormat/oracle2es.php
@@ -7,6 +7,7 @@ set_error_handler("exception_error_handler");
 
 function check_input(&$row) {
   $required_fields = array('hashtag_list', 'campaign', 'taskid');
+  $not_empty_fields = array('campaign', 'taskid');
 
   if (!is_array($row)) {
     fwrite(STDERR, "(WARN) Failed to decode message.\n");
@@ -16,6 +17,13 @@ function check_input(&$row) {
   foreach ($required_fields as $field) {
     if (!(array_key_exists($field, $row))) {
       fwrite(STDERR, "(WARN) Required field \"$field\" is missed.\n");
+      return FALSE;
+    }
+  }
+
+  foreach ($not_empty_fields as $field) {
+    if (!(array_key_exists($field, $row) && $row[$field])) {
+      fwrite(STDERR, "(WARN) Required field \"$field\" is empty.\n");
       return FALSE;
     }
   }
