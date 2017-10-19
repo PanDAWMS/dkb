80775381fd454872c61b9ca75ec1ea3d3e6999c4
Marina Golosova <golosova.marina@gmail.com>
2017-10-19 11:45:43 +0000

    DF/ora2es: make test script reset the starting OFFSET value.

--- Utils/Dataflow/run/ora2es.sh
+++ Utils/Dataflow/run/ora2es.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+START_OFFSET="09-05-2016 00:00:00"
+
 base_dir=$( cd "$(dirname "$(readlink -f "$0")")"; pwd)
 
 # Oracle Connector
@@ -11,6 +13,10 @@ cmd_19="${base_dir}/../../019_oracle2esFormat/oracle2es.php"
 
 # Stage 69
 cmd_69="${base_dir}/../../069_upload2es/load_data.sh"
+
+# Reset offset value
+sed -i.bak -e"s/^offset = .*$/offset = $START_OFFSET/" \
+    $cfg_OC
 
 # Run Oracle Connector
 BATCH_SIZE=100
