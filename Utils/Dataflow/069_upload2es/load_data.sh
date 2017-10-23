3dfd722f0b2a9045703f1041449da5b61d2b7051
Marina Golosova <golosova.marina@gmail.com>
2017-10-23 12:51:45 +0000

    DF/69: add possibility to load data in streaming mode
    Streaming mode means that the script (load_data.sh) is running
      in a non-stop cycle, reading input data from STDIN. We don`t
      want to run cURL for each and every input line, so we need
      to buffer input lines for a while to push them to the ES
      in a bulk mode.
    
    The signal to "stop buffering" comes from the outside; batches
      of lines are separated with a special symbol (hardcoded `\0`
      for now).
    
    The shebang was changed as `sh` claims that it doesn`t know about
      ioption `-d` for `read`.
    
    Can be tested with:
    ```
    echo -e -n "One line
    Second line
    third line
    \x00
    Line 1
    Line 2
    \x00" | ./load_data.sh
    ```

--- Utils/Dataflow/069_upload2es/load_data.sh
+++ Utils/Dataflow/069_upload2es/load_data.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 log() {
   echo "$(date): $*" >&2
 }
 
 usage() {
   echo "USAGE:
-$(basename "$0") FILE...
+$(basename "$0") [FILE...]
 
 PARAMETERS:
   FILE -- file in NDJSON format for loading to Elasticsearch via bulk interface
@@ -14,8 +14,6 @@ PARAMETERS:
 
 base_dir=$( cd "$( dirname "$( readlink -f "$0" )" )" && pwd )
 
-[ -z "$1" -o ! -f "$1" ] && log $(usage) && exit 1
- 
 ES_CONFIG="$(base_dir)/../config/es"
 log "Loading defaults and config $ES_CONFIG if any"
 ES_HOST='127.0.0.1'
@@ -24,8 +22,29 @@ ES_PORT='9200'
 [ -f "$ES_CONFIG" ] && source "$ES_CONFIG"
 [ -n "$ES_USER" -a "$ES_PASSWORD" ] && ES_AUTH="--user ${ES_USER}:${ES_PASSWORD}"
 
-log "Putting data to ES"
-for INPUTFILE in $*;
-do
-  curl $ES_AUTH "http://$ES_HOST:$ES_PORT/_bulk?pretty" --data-binary @${INPUTFILE} || exit 3
-done
+DELIMETER=`echo -e -n "\x00"`
+
+cmd="curl $ES_AUTH http://$ES_HOST:$ES_PORT/_bulk?pretty --data-binary @"
+
+load_files () {
+  [ -z "$1" -o ! -f "$1" ] && log $(usage) && exit 1
+
+  log "Putting data to ES"
+  for INPUTFILE in $*;
+  do
+    ${cmd}${INPUTFILE} || exit 3
+  done
+}
+
+load_stream () {
+  log "Switched to the stream mode."
+  while read -r -d "$DELIMITER" line; do
+    echo "$line" | ${cmd}-
+  done
+}
+
+if [ -z "$1" ] ; then
+  load_stream
+else
+  load_files $*
+fi
