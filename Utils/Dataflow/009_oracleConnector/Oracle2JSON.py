PanDAWMS/dkb

commit 071d66c41a5631332fb05e10823e89ea70861a13

DF/09: add config file checks before reading.
Made `--config` a required parameter of File('rw') type.

Utils/Dataflow/009_oracleConnector/Oracle2JSON.py
@@ -272,7 +272,8 @@ def get_category(row):
 
 def parsingArguments():
     parser = argparse.ArgumentParser(description='Process command line arguments.')
-    parser.add_argument('--config', help='Configuration file path')
+    parser.add_argument('--config', help='Configuration file path',
+                        type=argparse.FileType('rw'), required=True)
     parser.add_argument('--mode', help='Mode of execution: PLAIN | SQUASH',
                         choices=[PLAIN_POLICY, SQUASH_POLICY])
     return parser.parse_args()

commit 1af694715512c1be7af6e5a958e7822868a9dd84

DF/09: fix issue with ConfigParser (fixup for 071d66c)
Introduced in 071d66c (DF/09: add config file checks...)

We turned filename into File object, but used still as filename.

However `ConfigParser.readfp()` appeared to close file descriptor
  passed to it, so we could not reuse it to rewrite and re-read data.

We can`t avoid explicit checks, as ConfigParser does not provide details
  about errors occured due to the missed / unreadable file.

Thus the checks are to be performed manually.

Utils/Dataflow/009_oracleConnector/Oracle2JSON.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+import os
 import json
 import argparse
 import ConfigParser
@@ -290,9 +291,16 @@ def get_category(row):
 def parsingArguments():
     parser = argparse.ArgumentParser(description='Process command line arguments.')
     parser.add_argument('--config', help='Configuration file path',
-                        type=argparse.FileType('rw'), required=True)
+                        type=str, required=True)
     parser.add_argument('--mode', help='Mode of execution: PLAIN | SQUASH',
                         choices=[PLAIN_POLICY, SQUASH_POLICY])
+    args = parser.parse_args()
+    if not os.access(args.config, os.F_OK):
+        sys.stderr.write("argument --config: '%s' file not exists\n" % args.config)
+        sys.exit(1)
+    if not os.access(args.config, os.R_OK|os.W_OK):
+        sys.stderr.write("argument --config: '%s' read/write access failed\n" % args.config)
+        sys.exit(1)
     return parser.parse_args()
 
 if  __name__ == '__main__':