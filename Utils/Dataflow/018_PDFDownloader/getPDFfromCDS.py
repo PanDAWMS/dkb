PanDAWMS/dkb

commit 30ed558e0fcfbff9ce07898493c8778e73f6f166

DF/18: join Stages 013 and 020 into 018 (with HDFS functionality).
Issue: need to work with two Kerberos tickets from different realms.
For now tested in two parts: KIAE HDFS part and CERN part.

Utils/Dataflow/018_PDFDownloader/getPDFfromCDS.py
@@ -5,11 +5,64 @@ Stage 018: download PDF files from CDS and upload them to HDFS
 """
 
 import sys
+from urlparse import urlparse
+import subprocess
+import warnings
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
+
 sys.path.append("../")
 
 import pyDKB
 from pyDKB.dataflow import DataflowException
 from pyDKB.dataflow.stage import JSONProcessorStage
+from pyDKB.dataflow import CDSInvenioConnector, KerberizedCDSInvenioConnector
+from pyDKB.common import hdfs, HDFSException
+
+def transfer(url, hdfs_name):
+    """ Download file from given URL and upload to HDFS. """
+    cmd = ["./transferPDF.sh", url, hdfs_name]
+    try:
+        sp = subprocess.Popen(cmd, stdin=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   stdout=subprocess.PIPE)
+        if hdfs.check_stderr(sp):
+            raise subprocess.CalledProcessError(proc.returncode, cmd)
+        out = sp.stdout.readline()
+        if out:
+            out = out.strip()
+        return out
+    except (subprocess.CalledProcessError, OSError, HDFSException), err:
+        sys.stderr.write("Failed to transfer data from CDS to HDSF: %s\n"
+                         % err)
+        return None
+
+def get_recid(item):
+    """ Get CDS record ID. """
+    url = urlparse(item.get('url', ''))
+    if not url.netloc in ("cds.cern.ch", "cdsweb.cern.ch"):
+        return None
+    s = url.path.split('/')
+    if len(s) < 3 or not s[2].isdigit():
+        return None
+    return s[2]
+
+def get_url(item, cds):
+    """ Get URL of the document`s PDF from CDS. """
+    recid = get_recid(item)
+    if not recid:
+        return None
+    cds_results = cds.get_record(recid)
+    if not cds_results:
+        return None
+    # use ['8564_u'] instead of .get('8564_u') as it is a
+    # invenio_client.connector.Record and in list-dict terms the structure
+    # looks this way:
+    # { ..., "8564_": [{"y": ["Description", ...], "u": ["url", ...]}], ...}
+    urls = cds_results[0]['8564_u']
+    for url in urls:
+        if url.split('.')[-1].lower() == "pdf":
+            return url
+    return None
 
 def process(stage, msg):
     """ Message processing function.
@@ -17,19 +70,67 @@ def process(stage, msg):
     Input message: JSON
     Output message: JSON
     """
-    myMessage = stage.output_message_class()(msg.content())
-    stage.output(myMessage)
+    data = msg.content()
+    ARGS = stage.ARGS
+    for item in data.get('supporting_notes', []):
+        dkbID = item.get('dkbID')
+        url = get_url(item.get('GLANCE', {}), ARGS.cds)
+        if not (dkbID and url):
+            continue
+        hdfs_location = transfer(url, dkbID + ".pdf")
+        if not hdfs_location:
+            continue
+        out_data = {'dkbID': dkbID, 'PDF': "hdfs://" + hdfs_location}
+        out_msg = stage.output_message_class()(out_data)
+        stage.output(out_msg)
     return True
 
 def main(args):
     """ Main function. """
     stage = JSONProcessorStage()
+
+    stage.add_argument("-l", "--login", action="store", type=str, nargs='?',
+                       help="CERN account login",
+                       default='',
+                       const='',
+                       metavar="LOGIN",
+                       dest='login'
+                      )
+    stage.add_argument("-p", "--password", action="store", type=str, nargs='?',
+                       help="CERN account password",
+                       default='',
+                       const='',
+                       metavar="PASSWD",
+                       dest='password'
+                      )
+    stage.add_argument("-k", "--kerberos", action="store", type=bool, nargs='?',
+                       help="Use Kerberos-based authentification",
+                       default=False,
+                       const=True,
+                       metavar="KERBEROS",
+                       dest='kerberos'
+                      )
     stage.process = process
 
     exit_code = 0
     try:
         stage.parse_args(args)
-        stage.run()
+
+        if not stage.ARGS.login and not stage.ARGS.kerberos:
+            sys.stderr.write("WARNING: no authentication method"
+                             " will be used.\n")
+
+        warnings.simplefilter("once", InsecurePlatformWarning)
+        ARGS = stage.ARGS
+
+        if ARGS.kerberos:
+            Connector = KerberizedCDSInvenioConnector
+        else:
+            Connector = CDSInvenioConnector
+
+        with Connector(ARGS.login, ARGS.password) as cds:
+            ARGS.cds = cds
+            stage.run()
     except (DataflowException, RuntimeError), err:
         if str(err):
             sys.stderr.write("(ERROR) %s\n" % err)

Utils/Dataflow/018_PDFDownloader/README
@@ -34,3 +34,10 @@ JSON:
    "PDF":   <PDF location>
 }
 }}}
+
+Files
+=====
+./transferPDF.sh    service script to download PDF from CDS
+                      and upload it to HDFS.
+
+./getPDFfromCDS.py  main stage script

Utils/Dataflow/018_PDFDownloader/transferPDF.sh
@@ -1,0 +1,64 @@
+#!/bin/bash
+
+# Downloads PDF file from CDS and upload copy to the HDFS
+#
+# USAGE:
+#     ./transferPDF.sh URL [ REQUIRED_NAME ]
+
+PID=$$
+HDFS_DIR="/user/DKB/store/PDF"
+
+usage() {
+    echo "
+USAGE:
+    $(basename $0) url [req-name]
+
+PARAMETERS:
+    url      -- URL with the original file
+    req-name -- required file name to store in HDFS
+" >&2
+}
+
+upload() {
+    # Upload file to HDFS
+    [ -z "$1" ] && return 1
+    [ -z "$2" ] && hdfs_file=$HDFS_DIR/`basename $1` || hdfs_file=$HDFS_DIR/$2
+    hadoop fs -put -f $1 $HDFS_DIR/$2
+    echo $hdfs_file
+}
+
+
+download() {
+    # Download file from the original location
+    [ -z "$1" ] && return 1
+    url=$1
+    cookie=/tmp/cern-sso-cookie-$PID
+    tmpf=`mktemp /tmp/XXXXXXX.pdf`
+
+    cern-get-sso-cookie --krb -r -u "$url" -o $cookie 2>&1 >/dev/null
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to get CERN SSO cookie." >&2
+        exit 3
+    fi
+
+    curl -k -L -f -s  --cookie $cookie --cookie-jar $cookie $url -o $tmpf && echo $tmpf
+}
+
+which cern-get-sso-cookie 2>&1 >/dev/null
+
+if [ $? -ne 0 ]; then
+    echo "cern-get-sso-cookie is not installed.
+See http://linux.web.cern.ch/linux/docs/cernssocookie.shtml for details." >&2
+    exit 2
+fi
+
+if [ -z "$1" ]; then
+    usage
+    exit 1
+fi
+
+local_file=`download $1` \
+  && upload $local_file $2 2>/dev/null
+
+exit $?

commit 6bd4d5cf35970a0c1cd18054b3c34b63adce66d8

DF/18: fix wrong variable name
Introduced in 30ed558:
  DF/18: join Stages 013 and 020 into 018 (with HDFS functionality).

Utils/Dataflow/018_PDFDownloader/getPDFfromCDS.py
@@ -25,7 +25,7 @@ def transfer(url, hdfs_name):
                                    stderr=subprocess.PIPE,
                                    stdout=subprocess.PIPE)
         if hdfs.check_stderr(sp):
-            raise subprocess.CalledProcessError(proc.returncode, cmd)
+            raise subprocess.CalledProcessError(sp.returncode, cmd)
         out = sp.stdout.readline()
         if out:
             out = out.strip()