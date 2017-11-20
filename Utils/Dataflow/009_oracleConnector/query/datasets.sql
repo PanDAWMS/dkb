b1fca040470bb8aca1c75f12ee0287712ebe88a0
Maria <magsend@gmail.com>
2017-11-20 13:11:07 +0300

    Return only OUTPUT datasets
    Is was decided to substitute array of 'input' dataset to single 'primary_input' string.

--- Utils/Dataflow/009_oracleConnector/query/datasets.sql
+++ Utils/Dataflow/009_oracleConnector/query/datasets.sql
@@ -15,7 +15,7 @@ FROM
   LEFT JOIN t_task tt
   ON t.taskid = tt.taskid
 WHERE
-  jd.type IN ('input', 'output') AND
+  jd.type IN ('output') AND
       t.timestamp > to_date('%s', 'dd-mm-yyyy hh24:mi:ss') AND
       t.timestamp <= to_date('%s', 'dd-mm-yyyy hh24:mi:ss') AND
       t.pr_id > 300
