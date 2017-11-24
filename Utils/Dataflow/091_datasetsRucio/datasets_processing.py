0dd22c956b01b72cfdff61aaafc2b79ad0933c81
Marina Golosova <golosova.marina@gmail.com>
2017-11-24 13:01:35 +0000

    DF/91: documentation (comments, README, data samples).
    Added:
    - comments in datasets_processing.py script
    - README file
    - input / output examples files
    
    ---
    
    Original commits by Maria Grigorieva: 8df01df, 38c6e23

--- Utils/Dataflow/091_datasetsRucio/datasets_processing.py
+++ Utils/Dataflow/091_datasetsRucio/datasets_processing.py
@@ -18,6 +18,10 @@ def main():
 def process_data():
     input_file = open(INPUT)
     for line in input_file:
+        # Construct NDJSON string with the following format:
+        # { "taskid": <TASKID>,
+        #   "output": []
+        # }
         json_str = json.loads(line)
         ds = {}
         ds['taskid'] = json_str.get('taskid')
@@ -26,7 +30,17 @@ def process_data():
 
 
 def datasets_to_array(data, ds):
-
+    """
+    Constructs the array of dictionaries with datasets with the following format:
+    "output": [
+        {"deleted": true | false,
+         "datasetname": "<DS_NAME>",
+         "bytes": <BYTES>}
+    ]
+    :param data: input JSON string
+    :param ds: output dictionary
+    :return:
+    """
     ds[DS_TYPE] = []
 
     if data.get(DS_TYPE) is not None:
@@ -42,6 +56,9 @@ def datasets_to_array(data, ds):
                     ds_dict['deleted'] = False
                 ds[DS_TYPE].append(ds_dict)
             except:
+                # if dataset wasn't find in Rucio, it means that it was deleted from
+                # the Rucio catalog. In this case 'deleted' is set to TRUE and
+                # the length of file is set to -1
                 ds_dict['bytes'] = -1
                 ds_dict['deleted'] = True
                 ds[DS_TYPE].append(ds_dict)
@@ -49,22 +66,33 @@ def datasets_to_array(data, ds):
 
 
 def extract_scope(dsn):
-        if dsn.find(':') > -1:
-            return dsn.split(':')[0], dsn.split(':')[1]
-        else:
-            scope = dsn.split('.')[0]
-            if dsn.startswith('user') or dsn.startswith('group'):
-                scope = '.'.join(dsn.split('.')[0:2])
-            return scope, dsn
+    """
+    Extracs the first part of dataset name (ex: mc15_13TeV.XXX)
+    :param dsn: full dataset name
+    :return:
+    """
+    if dsn.find(':') > -1:
+        return dsn.split(':')[0], dsn.split(':')[1]
+    else:
+        scope = dsn.split('.')[0]
+        if dsn.startswith('user') or dsn.startswith('group'):
+            scope = '.'.join(dsn.split('.')[0:2])
+        return scope, dsn
 
 
 def get_metadata_attribute(rucio_client, dsn, attribute_name):
-        scope, dataset = extract_scope(dsn)
-        metadata = rucio_client.get_metadata(scope=scope, name=dataset)
-        if attribute_name in metadata.keys():
-            return metadata[attribute_name]
-        else:
-            return None
+    """
+    Returns the value of the attribute from Rucio
+    :param dsn: full dataset name
+    :param attribute_name: name of searchable attribute
+    :return:
+    """
+    scope, dataset = extract_scope(dsn)
+    metadata = rucio_client.get_metadata(scope=scope, name=dataset)
+    if attribute_name in metadata.keys():
+        return metadata[attribute_name]
+    else:
+        return None
 
 
 def parsingArguments():
