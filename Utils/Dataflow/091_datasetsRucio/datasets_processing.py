1c6a8009edc933a7362162122e18dc9eb0b7b2d0
Golosova Marina <golosova.marina@gmail.com>
2017-11-27 09:01:07 +0100

    DF/91: make dataset scope extraction more accurate.
    In theory, we can not have more than one colon in the dataset name.
    But it must be the problem of dataset naming, not ours: so we are taking
    the first part as `scope`, and leaving the rest as DS name.

--- Utils/Dataflow/091_datasetsRucio/datasets_processing.py
+++ Utils/Dataflow/091_datasetsRucio/datasets_processing.py
@@ -134,18 +134,20 @@ def extract_scope(dsn):
 
     Example:
       mc15_13TeV.XXX
-      mc15_13TeV:XXX.YYY
+      mc15_13TeV:YYY.XXX
 
     :param dsn: full dataset name
-    :return tuple: dataset scope (first field), rest of the name
+    :return tuple: dataset scope, dataset name
     """
-    if dsn.find(':') > -1:
-        return dsn.split(':')[0], dsn.split(':')[1]
+    pos = dsn.find(':')
+    if pos > -1:
+        result = (dsn[:pos], dsn.split[pos+1:])
     else:
         scope = dsn.split('.')[0]
         if dsn.startswith('user') or dsn.startswith('group'):
             scope = '.'.join(dsn.split('.')[0:2])
-        return scope, dsn
+        result = (scope, dsn)
+    return result
 
 
 def get_metadata_attribute(rucio_client, dsn, attribute_name):
