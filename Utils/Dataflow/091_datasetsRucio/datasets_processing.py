914d65c9e9576ca02c314de9d8b488ce8c27b12f
Golosova Marina <golosova.marina@gmail.com>
2017-11-27 09:37:24 +0100

    DF/91: refactoring (avoid passing global var as parameter)

--- Utils/Dataflow/091_datasetsRucio/datasets_processing.py
+++ Utils/Dataflow/091_datasetsRucio/datasets_processing.py
@@ -112,7 +112,7 @@ def datasets_to_array(data, ds):
             ds_dict = {}
             ds_dict['datasetname'] = dataset
             try:
-                bytes = get_metadata_attribute(rucio_client, dataset, 'bytes')
+                bytes = get_metadata_attribute(dataset, 'bytes')
                 if bytes == 'null' or bytes is None:
                     ds_dict['bytes'] = -1
                 else:
@@ -150,7 +150,7 @@ def extract_scope(dsn):
     return result
 
 
-def get_metadata_attribute(rucio_client, dsn, attribute_name):
+def get_metadata_attribute(dsn, attribute_name):
     """ Get attribute value from Rucio
 
     :param dsn: full dataset name
