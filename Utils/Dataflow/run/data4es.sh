cf8f74526d3aa040d13af458a0b93a719db41c1e
Golosova Marina <golosova.marina@gmail.com>
2017-11-28 16:19:43 +0100

    DF/data4es: refactoring (mainly comments and renamings).

--- Utils/Dataflow/run/data4es.sh
+++ Utils/Dataflow/run/data4es.sh
@@ -6,9 +6,12 @@ DEBUG=
 
 base_dir=$( cd "$(dirname "$(readlink -f "$0")")"; pwd)
 
+# Define commands to be used as dataflow nodes
+# ---
 # Oracle Connector
-cmd_OC="${base_dir}/../009_oracleConnector/Oracle2JSON.py"
-cfg_OC="${base_dir}/ora2json.cfg"
+cmd_09="${base_dir}/../009_oracleConnector/Oracle2JSON.py"
+cfg_09="${base_dir}/ora2json.cfg"
+cmd_OC="$cmd_09 --config $cfg_09 --mode SQUASH"
 
 # Stage 16
 cmd_16="${base_dir}/../016_task2es/task2es.py -m s"
@@ -18,31 +21,33 @@ cmd_19="${base_dir}/../019_esFormat/run.sh"
 
 # Stage 69
 cmd_69="${base_dir}/../069_upload2es/load_data.sh"
+# ---
 
-# Reset offset value
+# Reset offset value for Oracle Connector.
 [ -n "$START_OFFSET" ] \
     && sed -i.bak -e"s/^offset = .*$/offset = $START_OFFSET/" \
-           $cfg_OC
+           $cfg_09
 
-# Run Oracle Connector
+# Buffer for sink connector (Stage 69)
 buffer="${base_dir}/.record_buffer"
 touch $buffer || { echo "Failed to access buffer file." >&2; exit 2; }
 
-# EOP filter
+# Service (glue) functions
+# ---
+# EOP filter (required due to the unconfigurable EOP marker in pyDKB)
 eop_filter() {
   sed -e"s/\\x00//"
 }
 
+# Flush Sink Connector buffer to STDOUT and reset it
 flush_buffer() {
   cat $buffer
   echo -e '\x00'
   rm -f $buffer
 }
 
-oracle_connector() {
-  $cmd_OC --config $cfg_OC --mode "SQUASH"
-}
-
+# Glue between processing functions and Sink Connector
+# Buffers records and then send them as batch of $BATCH_SIZE
 mediator() {
   i=0
   while read -r line; do
@@ -53,12 +58,13 @@ mediator() {
   done
   flush_buffer
 }
+# ---
 
-# Run Oracle Connector
+# Run Dataflow
 
 [ -n "$DEBUG" ] \
-   && oracle_connector | tee oc.out | \
+   && $cmd_OC | tee oc.out | \
       $cmd_16 | eop_filter | tee 16.out | \
       $cmd_19 | tee 19.out | mediator > 69.inp \
-   || oracle_connector | $cmd_16 | eop_filter | \
+   || $cmd_OC | $cmd_16 | eop_filter | \
       $cmd_19 | mediator | $cmd_69
