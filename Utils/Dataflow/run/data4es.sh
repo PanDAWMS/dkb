a719f91bdb6830a3cc228f8da02dc87c68381532
Golosova Marina <golosova.marina@gmail.com>
2017-11-29 14:43:42 +0100

    DF/data4es: introduce branching (add Stage 091).
    Main changes
    ---
    1. Named pipes for branching
    To split Source Connector output in two, we got to write it in two
    different pipes, read by two different dataflow branches.
    
    2. Different output buffers for different branches
    
    3. Dataflow changed
    Was: 09->16->19->69
    Is:  09->16->19->69
          \->91->19->69
    
    TODO:
    Join branches at stage 19.
    Failed to do it now as output messages of 16 and 91 intermingled
      at 19`s input.

--- Utils/Dataflow/run/data4es.sh
+++ Utils/Dataflow/run/data4es.sh
@@ -6,6 +6,14 @@ DEBUG=
 
 base_dir=$( cd "$(dirname "$(readlink -f "$0")")"; pwd)
 
+# Create aux directories
+# ---
+pipe_dir=$base_dir/.pipe
+mkdir -p $pipe_dir
+
+buffer_dir=$base_dir/.buffer
+mkdir -p $buffer_dir
+
 # Define commands to be used as dataflow nodes
 # ---
 # Oracle Connector
@@ -21,16 +29,15 @@ cmd_19="${base_dir}/../019_esFormat/run.sh"
 
 # Stage 69
 cmd_69="${base_dir}/../069_upload2es/load_data.sh"
+
+# Stage 91
+cmd_91="${base_dir}/../091_datasetsRucio/datasets_processing.py -m s"
 # ---
 
 # Reset offset value for Oracle Connector.
 [ -n "$START_OFFSET" ] \
     && sed -i.bak -e"s/^offset = .*$/offset = $START_OFFSET/" \
            $cfg_09
-
-# Buffer for sink connector (Stage 69)
-buffer="${base_dir}/.record_buffer"
-touch $buffer || { echo "Failed to access buffer file." >&2; exit 2; }
 
 # Service (glue) functions
 # ---
@@ -39,8 +46,15 @@ eop_filter() {
   sed -e"s/\\x00//g"
 }
 
+# Buffer file name
+get_buffer() {
+  [ -n "$1" ] && name=$1 || name=default
+  echo $buffer_dir/$name
+}
+
 # Flush Sink Connector buffer to STDOUT and reset it
 flush_buffer() {
+  bufer=`get_buffer "$1"`
   cat $buffer
   echo -e '\x00'
   rm -f $buffer
@@ -49,22 +63,80 @@ flush_buffer() {
 # Glue between processing functions and Sink Connector
 # Buffers records and then send them as batch of $BATCH_SIZE
 mediator() {
+  buffer=`get_buffer "$1"`
   i=0
   while read -r line; do
     echo $line >> $buffer
     let i=$i+1
     let f=$i%$BATCH_SIZE
-    [ $f -eq 0 ] && flush_buffer
+    [ $f -eq 0 ] && flush_buffer "$1"
   done
-  flush_buffer
+  flush_buffer "$1"
 }
 # ---
 
+# Create named pipes for dataflow branching
+# ---
+branch() {
+  [ -z "$1" ] \
+    && echo "Branch name not specified." >&2\
+    && return 1
+
+  name="$pipe_dir/$1"
+  if [ -e "$name" ]; then
+    [ -p "$name" ] \
+      && { echo $name; return 0; } \
+      || name=`mktemp -u $name.XXXX`
+  fi
+
+  mkfifo $name \
+    || { echo "Failed to create named pipe for branch $1" >&2;
+         return 1; }
+
+  echo $name
+}
+
+b_91=`branch b_91` && b_16=`branch b_16` \
+  || exit $?
+
+# ---
+
 # Run Dataflow
+# ---
+
+# Source subchain: Oracle Connector
+source_chain() {
+  $cmd_OC | tee $b_16 $b_91
+}
+
+# Parallel subchains: 16 and 91
+chain_16() {
+  cat $b_16 | $cmd_16 | eop_filter
+}
+
+chain_91() {
+  cat $b_91 | $cmd_91 | eop_filter
+}
+
+# Sink chain
+sink_chain() {
+  [ -n "$DEBUG" ] \
+    && cmd="tee 69.$1.inp" \
+    || cmd=$cmd_69
+  $cmd_19 | mediator "$1" | $cmd > /dev/null
+}
+
+out=/dev/null
+[ -n "$DEBUG" ] \
+  && out='src.out'
+source_chain > $out &
 
 [ -n "$DEBUG" ] \
-   && $cmd_OC | tee oc.out | \
-      $cmd_16 | eop_filter | tee 16.out | \
-      $cmd_19 | tee 19.out | mediator > 69.inp \
-   || $cmd_OC | $cmd_16 | eop_filter | \
-      $cmd_19 | mediator | $cmd_69
+  && out='16.out'
+chain_16 | tee $out | sink_chain b16 &
+
+[ -n "$DEBUG" ] \
+  && out='91.out'
+chain_91 | tee $out | sink_chain b91 &
+
+wait
