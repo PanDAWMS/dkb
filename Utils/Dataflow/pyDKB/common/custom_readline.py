38418efba126a203bd08405b2b3e473e4899064b
Anastasiia Kaida <anastasiakaida@gmail.com>
2018-02-09 16:03:58 +0700

    pyDKB/fix: custom_readline() description

--- Utils/Dataflow/pyDKB/common/custom_readline.py
+++ Utils/Dataflow/pyDKB/common/custom_readline.py
@@ -4,10 +4,16 @@ import fcntl
 
 
 def custom_readline(f, newline):
-    """Custom readline() function. It separates content from a text file 'f'
+    """Custom readline() function.
+
+    Custom_readline() separates content from a text file 'f'
     by delimiter 'newline' to distinct messages.
     The last line can be incomplete, if the input data flow is interrupted
     in the middle of data writing.
+
+    Keyword arguments:
+    f -- file/stream to read
+    newline -- custom delimiter
     """
     poller = select.poll()
     poller.register(f, select.POLLIN)
