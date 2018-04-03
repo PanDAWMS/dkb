39d28453b841760a8f9404df36f98b2d79be08e3
Marina Golosova <golosova.marina@gmail.com>
2018-04-03 09:25:08 +0000

    pyDKB: add 'log()' method to AbstractStage.

--- Utils/Dataflow/pyDKB/dataflow/types.py
+++ Utils/Dataflow/pyDKB/dataflow/types.py
@@ -4,8 +4,9 @@ Type definitions for library objects.
 
 from ..common import Type
 
-__all__ = ["dataType", "messageType", "codeType"]
+__all__ = ["dataType", "messageType", "codeType", "logLevel"]
 
 dataType = Type("DOCUMENT", "AUTHOR", "DATASET")
 messageType = Type("STRING", "JSON", "TTL")
 codeType = Type("STRING")
+logLevel = Type("TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL")
