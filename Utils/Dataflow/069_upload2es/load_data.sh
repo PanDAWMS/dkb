db582bf83ebb0cd9e82cce61a7be568d06f86353
Anastasiia Kaida <anastasiakaida@gmail.com>
2018-04-27 01:48:13 +0700

    DF/69: custom EOP and EOB implementation
    EOP -- End-of-process
    EOB -- End-of-batch (old: $DELIMITER)
    
    For `read -d` in bash EOB (as a delimiter)  should be not more that one byte.
    
    The previous version of a script causes contradictions connected with
    `mode dependent` behaviour. Despite the fact that the arguments for
    determining custom markers are counted from the second, the script
    switches to a file mode only if there are no arguments at all. In
    that case we just can't set any custom marker for a file mode.

--- Utils/Dataflow/069_upload2es/load_data.sh
+++ Utils/Dataflow/069_upload2es/load_data.sh
@@ -12,6 +12,34 @@ PARAMETERS:
 "
 }
 
+while [[ $# > 1 ]]
+do
+  key="$2"
+  case $key in
+    -h|--help)
+      usage
+      exit
+      ;;
+    -E|--eop)
+      EOP="$3"
+      shift
+      ;;
+    -B|--eob)
+      EOB="$3"
+      shift
+      ;;
+    -*)
+      echo "(ERROR) Unknown option: $key" >&2
+      usage >&2
+      exit 1
+      ;;
+    *)
+      break
+      ;;
+  esac
+  shift
+done
+
 base_dir=$( cd "$( dirname "$( readlink -f "$0" )" )" && pwd )
 
 ES_CONFIG="${base_dir}/../../Elasticsearch/config/es"
@@ -24,8 +52,7 @@ ES_PORT='9200'
 
 CURL_N_MAX=10
 SLEEP=5
-DELIMITER=`echo -e -n "\x00"`
-EOProcess=`echo -e -n "\x06"`
+EOBatch="\x04"
 
 cmd="curl $ES_AUTH http://$ES_HOST:$ES_PORT/_bulk?pretty --data-binary @"
 
@@ -40,20 +67,35 @@ load_files () {
 }
 
 load_stream () {
-  log "Switched to the stream mode."
-  while read -r -d "$DELIMITER" line; do
-    n=`ps axf | grep '[c]url' | grep "$HOST:$PORT" | wc -l`
-    while [ $n -gt $CURL_N_MAX ]; do
-      sleep $SLEEP
+  if ((`echo -ne "$EOBatch" | wc -c` > 1)) ; then
+    log "Fail: switch to the stream mode."
+    echo "(ERROR) End-of-batch: too large."  >&2
+  else
+    log "Switched to the stream mode."
+    while read -r -d `echo -ne "$EOBatch"` line; do
       n=`ps axf | grep '[c]url' | grep "$HOST:$PORT" | wc -l`
+      while [ $n -gt $CURL_N_MAX ]; do
+        sleep $SLEEP
+        n=`ps axf | grep '[c]url' | grep "$HOST:$PORT" | wc -l`
+      done
+      echo "$line" | ${cmd}- &
+      echo -ne "$EOProcess"
     done
-    echo "$line" | ${cmd}- &
-    echo -n "$EOProcess"
-  done
+  fi
 }
 
 if [ -z "$1" ] ; then
+  if [ -z "$EOP" ] ; then
+    EOProcess="\0"
+  else
+    EOProcess="$EOP"
+  fi
   load_stream
 else
+  if [ -z "$EOP" ] ; then
+    EOProcess=""
+  else
+    EOProcess="$EOP"
+  fi
   load_files $*
 fi
