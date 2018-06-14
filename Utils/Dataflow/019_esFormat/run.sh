4f085db74d1409fb77eb049e2569fd73221ff4c1
Marina Golosova <golosova.marina@gmail.com>
2018-06-14 15:03:38 +0200

    DF/19: make config file configurable via command line.

--- Utils/Dataflow/019_esFormat/run.sh
+++ Utils/Dataflow/019_esFormat/run.sh
@@ -2,7 +2,34 @@
 
 base_dir=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
 
+usage() {
+  echo "USAGE:
+$(basename "$0") [-c CONFIG]
+  
+PARAMETERS:
+  CONFIG -- configuration file
+"
+}
+
 ES_CONFIG=$base_dir/../../Elasticsearch/config/es
+
+while [ -n "$1" ]; do
+  case "$1" in
+    --config|-c)
+      [ -n "$2" ] && ES_CONFIG="$2" || { usage >&2 && exit 1; }
+      shift;;
+    --)
+      shift
+      break;;
+    -*)
+      echo "Unknown option: $1" >&2
+      usage >&2
+      exit 1;;
+    *)
+      break;;
+  esac
+  shift
+done
 
 [ -r "$ES_CONFIG" ] \
   || { echo "Can't access configuration file: $ES_CONFIG" >&2 \
