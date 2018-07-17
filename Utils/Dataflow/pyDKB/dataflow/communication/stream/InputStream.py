b7953c19867b3ad68e5c74cd977822aed2fe9b42
Marina Golosova <golosova.marina@gmail.com>
2018-07-17 13:42:25 +0300

    pyDKB/*/communication: fix empty source detection (custom EOM).
    `FileConsumer.source_is_empty()`, used to check if current (file) data
    source is fully read, checked current cursor position against file size;
    but when custom EOM is used, `custom_readline()` reads data into local
    buffer and thus file can be fully read, while not all the values from
    the buffer are processed.
    
    To fix this issue new method `InputStream.is_readable()` is added with
    different implementations: for ordinar file reading (used to work with
    `'\n'` or empty EOM) and for `custom_readline()` generator.
    
    Now if current source is bound to a Stream object, it is checked via the
    Stream method; if not -- directly, as it used to be. File size is not
    saved in the `current_file` hash as it shouldn't be checked here that
    often as when there were no such functionality in the `InputStream`.

--- Utils/Dataflow/pyDKB/dataflow/communication/stream/InputStream.py
+++ Utils/Dataflow/pyDKB/dataflow/communication/stream/InputStream.py
@@ -8,6 +8,9 @@ from . import logLevel
 from . import Message
 from pyDKB.common import custom_readline
 
+import os
+import sys
+
 
 class InputStream(Stream):
     """ Implementation of the input stream. """
@@ -24,10 +27,13 @@ class InputStream(Stream):
         fd = self.get_fd()
         if self.EOM == '\n':
             self.__iterator = iter(fd.readline, "")
+            self.is_readable = self._fd_is_readable
         elif self.EOM == '':
             self.__iterator = iter(fd.read, "")
+            self.is_readable = self._fd_is_readable
         else:
             self.__iterator = custom_readline(fd, self.EOM)
+            self.is_readable = self._gi_is_readable
 
     def reset(self, fd, close=True, force=False):
         """ Reset current stream with new file descriptor.
@@ -40,6 +46,63 @@ class InputStream(Stream):
         if force or (old_fd and fd != old_fd):
             self._reset_iterator()
         return old_fd
+
+    def is_readable(self):
+        """ Check if current input stream is readable.
+
+        :returns: None  -- not initialized,
+                  False -- empty,
+                  True  -- not empty
+        :rtype: bool, NoneType
+        """
+        return self._unknown_is_readable()
+
+    def _unknown_is_readable(self):
+        """ Placeholder: readability test for not initialized stream.
+
+        This function is needed in case that we need to reset `is_readable`
+        and the whole Stream object back to the "undefined" state.
+
+        :returns: None
+        :rtype: NoneType
+        """
+        return None
+
+    def _fd_is_readable(self):
+        """ Check if bound file descriptor is readable.
+
+        :returns: None  -- not initialized,
+                  False -- empty,
+                  True  -- not empty
+        :rtype: bool, NoneType
+        """
+        fd = self.get_fd()
+        if not fd:
+            result = None
+        elif getattr(fd, 'closed', True):
+            result = False
+        elif fd.fileno() == sys.stdin.fileno():
+            result = True
+        else:
+            stat = os.fstat(fd.fileno())
+            result = fd.tell() != stat.st_size
+        return result
+
+    def _gi_is_readable(self):
+        """ Check if the generator iterator can return value on `next()` call.
+
+        :returns: False -- empty,
+                  True  -- not empty
+        :rtype: bool, NoneType
+        """
+        try:
+            return self.__iterator.send(True)
+        except StopIteration:
+            return False
+        except TypeError:
+            # If method 'next()' was never called yet,
+            # sending anything but None raises TypeError
+            return self._fd_is_readable()
 
     def parse_message(self, message):
         """ Verify and parse input message.
