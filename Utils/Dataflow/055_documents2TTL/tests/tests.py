06161f40a9a9bf520dbe640680b8a0a87cde9e49
Evildoor <evildoor256@gmail.com>
2018-08-07 15:56:08 +0300

    Added sample tests to stage 055 to use as examples.

---
+++ Utils/Dataflow/055_documents2TTL/tests/tests.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+"""
+Sample tests for Stage 055 to be used as example for writing tests.
+Usage: 'python -m unittest discover' from ..(directory with Stage 055 code).
+"""
+
+import unittest
+import documents2ttl
+
+
+class SimpleTestCase(unittest.TestCase):
+    def test_get_document_iri(self):
+        doc_id = "abcd"
+        iri = "<%s/document/%s>" % (documents2ttl.GRAPH, doc_id)
+        self.assertEqual(documents2ttl.get_document_iri(doc_id), iri)
+
+    def test_documents_links_manual(self):
+        data = {"dkbID": "0", "supporting_notes": [{"dkbID": "1"},
+                                                   {"dkbID": "2"}]}
+        result = "<http://nosql.tpu.ru:8890/DAV/ATLAS/document/0> "\
+                 "<http://nosql.tpu.ru/ontology/ATLAS#isBasedOn> "\
+                 "<http://nosql.tpu.ru:8890/DAV/ATLAS/document/1> .\n"\
+                 "<http://nosql.tpu.ru:8890/DAV/ATLAS/document/0> "\
+                 "<http://nosql.tpu.ru/ontology/ATLAS#isBasedOn> "\
+                 "<http://nosql.tpu.ru:8890/DAV/ATLAS/document/2> .\n"
+        self.assertEqual(documents2ttl.documents_links(data), result)
+
+    def test_documents_links_constructed(self):
+        id0 = "2"
+        ids = ["8", "3", "6"]
+        data = {"dkbID": id0, "supporting_notes": [{"dkbID": i} for i in ids]}
+        result = ""
+        iri0 = documents2ttl.get_document_iri(id0)
+        for note in data["supporting_notes"]:
+            iri = documents2ttl.get_document_iri(note["dkbID"])
+            result += "%s <%s#isBasedOn> %s .\n" % (iri0,
+                                                    documents2ttl.ONTOLOGY,
+                                                    iri)
+        self.assertEqual(documents2ttl.documents_links(data), result)
+
+    def test_fix_string_wrong_type(self):
+        s = 1
+        self.assertEqual(documents2ttl.fix_string(s), s)
+
+    def test_fix_string_backslash_n(self):
+        s = "\n"
+        fixed_s = "\\\\n"
+        self.assertEqual(documents2ttl.fix_string(s), fixed_s)
+
+    def test_fix_string_single_quote(self):
+        s = "'"
+        fixed_s = "\\\\'"
+        self.assertEqual(documents2ttl.fix_string(s), fixed_s)
+
+    def test_fix_string_backslash_single_quote(self):
+        s = "\'"
+        fixed_s = "\\\\'"
+        self.assertEqual(documents2ttl.fix_string(s), fixed_s)
+
+    def test_fix_string_backslash_double_quote(self):
+        # Is it normal that this and previous tests are so different?
+        s = "\""
+        fixed_s = ""
+        self.assertEqual(documents2ttl.fix_string(s), fixed_s)
+
+    def test_fix_string_string_without_characters_to_escape(self):
+        s = "Am I supposed to write something _important_/*smart* here?"\
+            "Preposterous!"
+        self.assertEqual(documents2ttl.fix_string(s), s)
+
+
+test_cases = (SimpleTestCase, )
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    for case in test_cases:
+        suite.addTest(loader.loadTestsFromTestCase(case))
+    return suite
