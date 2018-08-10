aa7f30a8d32ce40a821f82b99c5e3215e2ba3953
Anastasiia Kaida <anastasiakaida@gmail.com>
2018-08-11 05:55:47 +0700

    DF/69: custom EOP/EOB implementation
    *Adding help
    *Following options were implemented:
    
    EOP -- End-of-process
    EOB -- End-of-batch (old: $DELIMITER)
    BATCHMODE -- an option to support batch-mode processing.
    
    For `read -d` in bash EOB (as a delimiter)  should be not more that one byte.
    For batch-mode processing that option should be enabled:
    
    -b|--batch (e)enabled|(d)isabled
    Default: disabled
    
    If EOB (-B|--eob) is specifiend with a custom marker
    (not "\x17" -- default), batch-mode will be enabled anyway.
    
    In other cases batch-mode should be enabled.
    Note: Empty EOB = disabled batch mode.

--- Utils/Dataflow/069_upload2es/load_data.sh
+++ Utils/Dataflow/069_upload2es/load_data.sh
@@ -10,6 +10,17 @@ $(basename "$0") [-c CONFIG] [FILE...]
 PARAMETERS:
   CONFIG -- configuration file
   FILE   -- file in NDJSON format for loading to Elasticsearch via bulk interface
+
+OPTIONS:
+  -b, --batch {e[abled]|d[isabled]} Specifies batch-mode: (e)nabled|(d)isabled.
+  -B, --eob <EOB>                   Specifies the delimiter between sets of input
+                                    data in the stream mode.
+                                    Default: \x17
+                                    No batch-mode: \n
+  -E, --eop <EOP>                   Specifies end-of-process marker.
+                                    Default:
+                                    * in a (f)ile mode: none
+                                    * in a (s)tream mode: \0
 "
 }
 
@@ -19,8 +30,20 @@ ES_CONFIG="${base_dir}/../../Elasticsearch/config/es"
 
 while [ -n "$1" ]; do
   case "$1" in
-    --config|-c)
+    -c|--config)
       [ -n "$2" ] && ES_CONFIG="$2" || { usage >&2 && exit 1; }
+      shift;;
+    -h|--help)
+      usage
+      exit;;
+    -E|--eop)
+      EOP="$2"
+      shift;;
+    -B|--eob)
+      EOB="$2"
+      shift;;
+    -b|--batch)
+      BATCHMODE="${2,,}"
       shift;;
     --)
       shift
@@ -44,8 +67,22 @@ ES_PORT='9200'
 
 CURL_N_MAX=10
 SLEEP=5
-DELIMITER=`echo -e -n "\x00"`
-EOProcess=`echo -e -n "\x06"`
+EOBatch='\x17'
+BATCHMODE="d"
+
+case $BATCHMODE in
+  e|enabled)
+    [ -z "$EOB" ] && EOBatch="\n"
+    ;;
+  d|disabled)
+    ( [ -z "$EOB" ] || [ $EOB == "\x17" ] || [ $EOBatch == "\x17" ] ) && EOBatch="\n"
+    ( [ -n "$EOB" ] && [ ! $EOB == "\x17" ] ) && EOBatch="$EOB"
+    ;;
+  *)
+    log "Unexpected batch-mode parameter." && { [ -n "$BATCHMODE" ] && usage && return 2; }
+    break
+    ;;
+esac
 
 cmd="curl $ES_AUTH http://$ES_HOST:$ES_PORT/_bulk?pretty --data-binary @"
 
@@ -60,20 +97,35 @@ load_files () {
 }
 
 load_stream () {
-  log "Switched to the stream mode."
-  while read -r -d "$DELIMITER" line; do
-    n=`ps axf | grep '[c]url' | grep "$HOST:$PORT" | wc -l`
-    while [ $n -gt $CURL_N_MAX ]; do
-      sleep $SLEEP
+  if ((`echo -ne "$EOBatch" | wc -c` > 1)) ; then
+    log "Fail to switch to the stream mode. "`
+               `"End-of-batch marker is too long (> 1 byte)."
+  else
+    log "Switched to the stream mode."
+    while read -r -d `echo -ne "$EOBatch"` line; do
       n=`ps axf | grep '[c]url' | grep "$HOST:$PORT" | wc -l`
+      while [ $n -gt $CURL_N_MAX ]; do
+        sleep $SLEEP
+        n=`ps axf | grep '[c]url' | grep "$HOST:$PORT" | wc -l`
+      done
+      echo "$line" | ${cmd}- &
+      echo -ne "$EOProcess"
     done
-    echo "$line" | ${cmd}- &
-    echo -n "$EOProcess"
-  done
+  fi
 }
 
 if [ -z "$1" ] ; then
+  if [ -z "$EOP" ] ; then
+    EOProcess="\0"
+  else
+    EOProcess="$EOP"
+  fi
   load_stream
 else
+  if [ -z "$EOP" ] ; then
+    EOProcess=""
+  else
+    EOProcess="$EOP"
+  fi
   load_files $*
 fi
