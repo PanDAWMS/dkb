946f74bbdb360f9f6fb7424e87e71dd894ff280d
Marina Golosova <golosova.marina@gmail.com>
2018-08-17 14:09:56 +0000

    docs/pyDKB: improve docstrings (common.custom_readline).

--- Utils/Dataflow/pyDKB/common/custom_readline.py
+++ Utils/Dataflow/pyDKB/common/custom_readline.py
@@ -1,19 +1,40 @@
+"""
+Implementation of "readline"-like functionality for custom separator.
+
+.. todo:: make import of ``fcntl`` (or of this module) optional
+  to avoid errors when library is used under Windows.
+"""
+
 import select
 import os
 import fcntl
 
 
 def custom_readline(f, newline):
-    """Custom readline() function.
+    """ Read lines with custom line separator.
 
-    Custom_readline() separates content from a text file 'f'
-    by delimiter 'newline' to distinct messages.
-    The last line can be incomplete, if the input data flow is interrupted
-    in the middle of data writing.
+    Construct generator with readline-like functionality:
+    with every call of ``next()`` method it will read data from ``f``
+    untill the ``newline`` separator is found; then yields what was read.
 
-    Keyword arguments:
-    f -- file/stream to read
-    newline -- custom delimiter
+    .. warning:: the last line can be incomplete, if the input data flow
+      is interrupted in the middle of data writing.
+
+    :param f: readable file object
+    :type f: file
+    :param newline: delimeter to be used instead of ``\\n``
+    :type newline: str
+
+    :return: iterable object
+    :rtype: generator
+
+    .. todo::
+      * make last "line" handling more strict: no ``newline`` == no line;
+      * rethink function name (as "line" is actually a "message");
+      * move functionality to ``pyDKB.dataflow.communication`` [1]_
+        submodule)
+
+    .. [1] https://github.com/PanDAWMS/dkb/pull/129
     """
     poller = select.poll()
     poller.register(f, select.POLLIN)
