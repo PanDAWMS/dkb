2f8b45d8f0e49bdb5c1d0672b2621129c01c281a
Marina Golosova <golosova.marina@gmail.com>
2018-08-21 16:44:44 +0300

    API/setup: remove rubbish piece of code.

--- Utils/API/server/setup.sh
+++ Utils/API/server/setup.sh
@@ -180,9 +180,6 @@ build_www() {
 }
 
 install_www() {
-  [ ! $# -eq 0 ] \
-    && echo "Failed to ensure directory configuration ($WWW_DIR)" \
-    && exit 1
   [ ! -d "$build_dir" ] && _build
   [ ! -r "$base_dir/.files" ] \
     && echo "Config file '.files' is missed in '$base_dir'." \
