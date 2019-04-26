d54f90dfb25a94b001d29eb888e931ba1f42b5fd
Marina Golosova <golosova.marina@gmail.com>
2019-04-26 12:53:05 +0200

    pyDKB/storages: add interface methods for custom query usage.

--- Utils/Dataflow/pyDKB/storages/Storage.py
+++ Utils/Dataflow/pyDKB/storages/Storage.py
@@ -72,3 +72,64 @@ class Storage(object):
         :rtype: dict
         """
         raise NotImplementedError
+
+    def read_query(self, fname, qname=None):
+        """ Read query from file and save it.
+
+        :param fname: file name
+        :type fname: str
+        :param qname: query name (for futher usage)
+        :type qname: str
+        """
+        raise NotImplementedError
+
+    def save_query(self, query, qname=None):
+        """ Save query for further usage.
+
+        :param query: query content
+        :type query: object
+        :param qname: query name (must not start with '__')
+        :type qname: str
+        """
+        raise NotImplementedError
+
+    def get_query(self, qname):
+        """ Get query by name.
+
+        :param qname: query name (if None, last stored/used query will be used)
+        :type qnmae: str
+
+        :return: stored query
+        :rtype: object
+        """
+        raise NotImplementedError
+
+    def exec_query(self, qname=None, **kwargs):
+        """ Execute stored query with given parameters.
+
+        :param qname: query name (if None, last used/read
+                      one will be used)
+        :type qname: str, NoneType
+        :param kwargs: query parameters (applied with old-style
+                       string formatting operator '%')
+        :type kwargs: dict
+
+        :return: storage response
+        :rtype: object
+        """
+        raise NotImplementedError
+
+    def execute(self, query, **kwargs):
+        """ Execute query with given parameters.
+
+        :param query: query content
+        :type query: object
+        :param kwargs: query parameters (applied with old-style
+                       string formatting operator '%')
+        :type kwargs: dict
+
+        :return: storage response
+        :rtype: object
+        """
+        self.save_query(query)
+        return self.exec_query(**kwargs)
