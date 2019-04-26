b777e0add6f2fa42faf74e799fdb33140881eff1
Marina Golosova <golosova.marina@gmail.com>
2019-04-26 09:45:09 +0200

    pyDKB/storages: add methods to the Storage interface class (`log()`, `client()`).
    * `log()` -- quite useful method for all the derived classes;
    * `client()` -- given that one day we may want to make multiple
      simultaneous requests to the storage, it doesn'y look good to use
      instance variable `c` directly; method that returns ready-to-use client
      seems to be a better way.

--- Utils/Dataflow/pyDKB/storages/Storage.py
+++ Utils/Dataflow/pyDKB/storages/Storage.py
@@ -2,6 +2,11 @@
 pyDKB.storages.Storage
 """
 
+import sys
+from datetime import datetime
+
+from exceptions import StorageNotConfigured
+
 
 class Storage(object):
     """ Interface class for external and internal DKB storages. """
@@ -23,6 +28,16 @@ class Storage(object):
         """
         self.name = name
 
+    def log(self, level, message):
+        """ Output log message. """
+        if level not in ('TRACE', 'DEBUG', 'INFO', 'WARN', 'WARNING', 'ERROR',
+                         'CRITICAL'):
+            level = 'INFO'
+        dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        sys.stderr.write('(%s) %s (%s) %s\n' % (level, dt,
+                                                self.__class__.__name__,
+                                                message))
+
     def configure(self, cfg):
         """ Apply storage configuration (initialize client).
 
@@ -30,6 +45,18 @@ class Storage(object):
         :type cfg: dict
         """
         raise NotImplementedError
+
+    def client(self):
+        """ Get storage client.
+
+         Raise ``StorageNotConfigured`` if called before configuration.
+
+        :return: client object, corresponding given storage type.
+        :rtype: object
+        """
+        if not self.c:
+            raise StorageNotConfigured(self.name)
+        return self.c
 
     def get(self, id, **kwargs):
         """ Get object / record from storage by ID.
