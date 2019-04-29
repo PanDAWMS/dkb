fd4ae858252c17f1358fa44cf8c87a1f5cfafbbe
Marina Golosova <golosova.marina@gmail.com>
2019-04-29 13:55:15 +0300

    API/task_hist: improve undefined end_time handling.
    As undefined `end_time` was taken as 0 (1970-01-01 00:00:00), such tasks
    fall out of the distribution.
    Now for such tasks `end_time` is set to current timestamp (in UTC).

--- Utils/API/server/lib/dkb/api/storages/es.py
+++ Utils/API/server/lib/dkb/api/storages/es.py
@@ -8,6 +8,7 @@ import os
 import traceback
 import json
 from datetime import datetime
+import time
 
 from ..exceptions import DkbApiNotImplemented
 from exceptions import (StorageClientException,
@@ -188,6 +189,7 @@ def _raw_task_steps_hist(**kwargs):
     """
     logging.debug("_raw_task_steps_hist(%s)" % kwargs)
     q = dict(TASK_KWARGS)
+    kwargs['current_ts_ms'] = int(time.mktime(datetime.utcnow().timetuple())) * 1000
     q['body'] = get_query('task-steps-hist', **kwargs)
     if kwargs.get('start'):
         r = {"range": {"end_time": {"gte":
