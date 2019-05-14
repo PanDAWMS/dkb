aa25c0315969cbc5f8851b8e182d2f42481381af
Evildoor <evildoor256@gmail.com>
2019-05-14 17:25:50 +0300

    Update descriptions for utils' functions.

--- Utils/Dataflow/PDF2json/utils.py
+++ Utils/Dataflow/PDF2json/utils.py
@@ -9,15 +9,29 @@ import sys
 
 
 def path_join(a, b):
-    """ Wrapper around os.path.join.
+    ''' Wrapper around os.path.join.
     This wrapper is required to account for possible different
     separators in paths.
-    """
+
+    :param a: first path to join
+    :type a: str
+    :param b: second path to join
+    :type b: str
+
+    :return: resulting path
+    :rtype: str
+    '''
     return os.path.join(a, b).replace("\\", "/")
 
 
 def log(msg, prefix='DEBUG'):
-    ''' Add prefix and current time to message and write it to stderr. '''
+    ''' Add prefix and current time to message and write it to stderr.
+
+    :param msg: message
+    :type msg: str
+    :param prefix: log level indicating the type/importance of the message.
+    :type prefix: str
+    '''
     prefix = '(%s)' % (prefix)
     # 11 = len("(CRITICAL) "), where CRITICAL is the longest log level name.
     prefix = prefix.ljust(11)
