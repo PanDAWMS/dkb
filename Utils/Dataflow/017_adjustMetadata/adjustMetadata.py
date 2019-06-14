b265e10427d9c9d8a8e553073c3c6380f36769a9
Evildoor <evildoor256@gmail.com>
2019-06-14 15:57:44 +0200

    Adapt stage 017 to new version of pyDKB.

--- Utils/Dataflow/017_adjustMetadata/adjustMetadata.py
+++ Utils/Dataflow/017_adjustMetadata/adjustMetadata.py
@@ -20,8 +20,8 @@ try:
     dkb_dir = os.path.join(base_dir, os.pardir)
     sys.path.append(dkb_dir)
     import pyDKB
-    from pyDKB.dataflow.stage import JSONProcessorStage
-    from pyDKB.dataflow.messages import JSONMessage
+    from pyDKB.dataflow.stage import ProcessorStage
+    from pyDKB.dataflow.communication.messages import JSONMessage
     from pyDKB.dataflow.exceptions import DataflowException
 except Exception, err:
     sys.stderr.write("(ERROR) Failed to import pyDKB library: %s\n" % err)
@@ -234,28 +234,17 @@ def process(stage, message):
 
 def main(args):
     """ Program body. """
-    stage = JSONProcessorStage()
+    stage = ProcessorStage()
+    stage.set_input_message_type(JSONMessage.msg_type)
+    stage.set_output_message_type(JSONMessage.msg_type)
     stage.process = process
 
-    exit_code = 0
-    exc_info = None
-    try:
-        stage.parse_args(args)
-        stage.run()
-    except (DataflowException, RuntimeError), err:
-        if str(err):
-            sys.stderr.write("(ERROR) %s\n" % err)
-        exit_code = 2
-    except Exception:
-        exc_info = sys.exc_info()
-        exit_code = 3
-    finally:
-        stage.stop()
+    stage.configure(args)
 
-    if exc_info:
-        trace = traceback.format_exception(*exc_info)
-        for line in trace:
-            sys.stderr.write("(ERROR) %s" % line)
+    exit_code = stage.run()
+
+    if exit_code == 0:
+        stage.stop()
 
     exit(exit_code)
 
