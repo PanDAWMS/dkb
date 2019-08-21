c47ceabda39103fb0661197201bf4581f4867af7
Evildoor <evildoor256@gmail.com>
2019-08-21 14:02:38 +0200

    Fix the interruption of a cycle.
    Per Python documentation, "(continue statement) continues with the next cycle
    of the nearest enclosing loop". Therefore, "continue" makes no sense as
    the last line in the loop. It seems that "continue" was mistakenly used in
    place of "break": the goal of the inner loop is to locate two "fields", one
    with the parameter's name and the other with its value. For example, the
    processing of the following fields:
    
        ...
        {
          "@table": "dataset_physics_bridge",
          "@name": "insert_time",
          "@type": "TIMESTAMP(11)",
          "$": "2016-10-26 01:16:59"
        },
        {
          "@table": "physicsparameterdefs",
          "@name": "paramName",
          "@type": "VARCHAR2(128)",
          "$": "genFiltEff"
        },
        {
          "@table": "dataset_physics_bridge",
          "@name": "paramValue",
          "@type": "VARCHAR2(128)",
          "$": "1.0000E+00"
        },
        ...
    
    will result in the assignment:
    
        data["genFiltEff"] = "1.0000E+00"
    
    Due to this and the assumption that each "field" can only be used once in each
    row, replace "continue" with "break": why check the remaining fields when the
    two necessary are already located?
    Remove the line setting variables to None: with "break" the outer loop will
    proceed to next cycle and they will be set to None there.

--- Utils/Dataflow/095_datasetInfoAMI/amiDatasets.py
+++ Utils/Dataflow/095_datasetInfoAMI/amiDatasets.py
@@ -181,8 +181,7 @@ def amiPhysValues(data):
                     p_val = field['$']
                 if p_name and p_val:
                     data[p_name] = p_val
-                    p_name, p_val = None, None
-                    continue
+                    break
         return True
     except Exception as e:
         sys.stderr.write("(ERROR) Failed to process dataset '%s': "
