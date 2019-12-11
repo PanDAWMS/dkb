77fc87998589f0f402704ded9967c730feca86d2
Evildoor <evildoor256@gmail.com>
2019-12-11 14:57:29 +0100

    Add a hint to warning message.
    Incorrect credentials and their handling by AMI were the reason for creating
    this check. Mention them to (possibly) help finding the problem.

--- Utils/Dataflow/095_datasetInfoAMI/amiDatasets.py
+++ Utils/Dataflow/095_datasetInfoAMI/amiDatasets.py
@@ -98,7 +98,8 @@ def init_ami_client(userkey='', usercert=''):
         ami_client.execute('ListCommands')
     except Exception as e:
         sys.stderr.write("(WARN) Failed to perform test command "
-                         "ListCommands. Exception: %s\n" % str(e))
+                         "ListCommands. Are you sure you have"
+                         " a valid certificate?\nException: %s\n" % str(e))
 
 
 def get_ami_client():
