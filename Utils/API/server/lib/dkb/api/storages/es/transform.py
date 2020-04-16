0572217a01c04a7e172c9e78864fb70d6d7c1fe6
Marina Golosova <golosova.marina@gmail.com>
2020-04-16 15:18:10 +0300

    API/campaign_stat: improve 'input_events' calculation.
    We must make sure that we calculate the total number of input events
    only for "valid" tasks (those that were not cancelled, failed, broken or
    aborted for some reason). Invalid tasks are very likely to be replaced
    with the valid ones, and we don't want to take the number of input
    events from both of them.

--- Utils/API/server/lib/dkb/api/storages/es/transform.py
+++ Utils/API/server/lib/dkb/api/storages/es/transform.py
@@ -399,7 +399,9 @@ def campaign_stat(stat_data, events_src=None):
                             .get('value', None)
         esp_o = esp_o[events_src] if events_src != 'all' else esp_o
 
-        eps = {'input': step.get('input_events', {}).get('value', None),
+        eps = {'input': step.get('input_events', {})
+                            .get('input_events', {})
+                            .get('value', None),
                'output': esp_o,
                'ratio': None
                }
