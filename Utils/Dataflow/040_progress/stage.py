75f37823e911699ae133638cea849970c985391e
Marina Golosova <golosova.marina@gmail.com>
2020-06-25 18:22:10 +0200

    DF/data4es/040: stage implementation.

--- Utils/Dataflow/040_progress/stage.py
+++ Utils/Dataflow/040_progress/stage.py
@@ -11,6 +11,8 @@ Authors:
 import os
 import sys
 
+from datetime import datetime, timedelta
+
 try:
     base_dir = os.path.dirname(__file__)
     dkb_dir = os.path.join(base_dir, os.pardir)
@@ -25,6 +27,81 @@ except Exception, err:
     sys.exit(1)
 
 
+DATE_FORMAT = "%d-%m-%Y %H:%M:%S"
+
+
+def round_ts(timestamp, granularity):
+    """ Round timestamp to specified granularity.
+
+    :param timestamp: task timestamp (string in DATE_FORMAT)
+    :type timestamp: str
+    :param granularity: granularity of metadata snapshots.
+                        Value is a number of seconds:
+                         - N days (divisible by 86400);
+                         - part of a day (aliquot of 86400).
+                        Seconds will count from the beginning of day,
+                        and days -- from ``datetime.min``
+    :type granularity: int
+
+    :return: rounded timestemp
+    :rtype: datetime
+    """
+    dt = datetime.strptime(timestamp, DATE_FORMAT)
+    td_gran = timedelta(seconds=granularity)
+    if (td_gran.days and td_gran.seconds) \
+            or not td_gran:
+        raise DataflowException("Invalid progress granularity: %s (%s d,"
+                                " %s s). Expected non-zero number divisible"
+                                " by or aliquot of 86400 (N days or an aliqout"
+                                " part of one day).")
+    if td_gran.days:
+        days = (dt - dt.min).days / td_gran.days * td_gran.days
+        new_ts = dt.min + timedelta(days=days)
+    else:
+        s = (dt - dt.min).seconds / td_gran.seconds * td_gran.seconds
+        new_ts = dt.min + timedelta(seconds=s)
+
+    return new_ts
+
+
+def progress_data(data, granularity=86400):
+    """ Generate document with task progress metadata.
+
+    :param data: task metadata
+    :type data: dict
+    :param granularity: granularity of metadata snapshots.
+                        Value is a number of seconds:
+                         - N days (divisible by 86400);
+                         - part of a day (aliquot of 86400).
+                        Seconds will count from the beginning of day,
+                        and days -- from ``datetime.min``
+    :type granularity: int
+
+    :return: task current progress metadata
+    :rtype: dict
+    """
+    result = {}
+    fields = ['hashtag_list', 'ami_tags_format_step',
+              'ctag_format_step']
+    # Required fields
+    result['taskid'] = data['taskid']
+    rounded_ts = round_ts(data['task_timestamp'], granularity)
+    result['date'] = rounded_ts.strftime(DATE_FORMAT)
+    # "Optional" fields
+    for f in fields:
+        result[f] = data.get(f)
+    result['processed_events'] = data.get('processed_events_v2')
+    result['mc_step'] = data.get('step_name')
+    # Service fields
+    rounded_ts_sec = int((rounded_ts - datetime(1970, 1, 1)).total_seconds())
+    rounded_ts_ms = rounded_ts_sec * 10**3
+    result['_id'] = '%s_%s' % (rounded_ts_ms, result['taskid'])
+    result['_type'] = 'task_progress'
+    result['_index'] = 'progress'
+
+    return result
+
+
 def process(stage, message):
     """ Single message processing.
 
@@ -36,7 +113,11 @@ def process(stage, message):
     :returns: True or False in case of failure
     :rtype: bool
     """
+    data = message.content()
+    out_data = progress_data(data)
+    out_message = JSONMessage(out_data)
     stage.output(message)
+    stage.output(out_message)
     return True
 
 
