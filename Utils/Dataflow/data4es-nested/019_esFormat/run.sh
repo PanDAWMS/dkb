PanDAWMS/dkb

commit 70bb8e982b0c5ecb94a3dbbeb722cc13e3dd7036

Change Dataflow directory layout.
Stage directories are moved to dataflow-dedicated subdirectories:
  Dataflow/data4es
  Dataflow/docs4virtuoso

Stages configuration files (Dataflow/config) are moved together with
stages (to Dataflow/<df_name>/config).

DF management scripts (Dataflow/run/...) are also moved (to
Dataflow/<df_name>/run), but symlinks are created instead (for
compatibility).

Common libraries are left in Dataflow/, but symlinks are created:
  Dataflow/<df_name>/shell_lib
  Dataflow/<df_name>/pyDKB

---

After this commit any command, used to manage metadata integration
process (specifically, data4es) must keep on working as if nothing has
changed.

Utils/Dataflow/data4es/run/data4es-consistency-check
@@ -1,0 +1,60 @@
+#!/bin/bash -l
+
+DEBUG=
+
+base_dir=$( cd "$(dirname "$(readlink -f "$0")")"; pwd)
+lib="$base_dir/../shell_lib"
+
+# Directories with configuration files
+[ -n "$DATA4ES_CONSISTENCY_CONFIG_PATH" ] && \
+    CONFIG_PATH="$DATA4ES_CONSISTENCY_CONFIG_PATH" || \
+    CONFIG_PATH="${base_dir}/../config:${base_dir}/../../Elasticsearch/config"
+
+source $lib/get_config
+source $lib/eop_filter
+
+# Oracle
+cfg009=`get_config "consistency009.cfg"`
+cmd_009="$base_dir/../009_oracleConnector/Oracle2JSON.py --config $cfg009"
+
+# Formatting
+cmd_016="$base_dir/../016_task2es/task2es.py -m s"
+
+# ES
+cfg_es=`get_config "es"`
+cmd_071="$base_dir/../071_esConsistency/consistency.py -m s --conf $cfg_es"
+
+while [ -n "$1" ]; do
+  case $1 in
+    --debug)
+      DEBUG=1;;
+    *)
+      break;;
+  esac
+  shift
+done
+
+out_009=/dev/null
+out_016=/dev/null
+out_071=/dev/null
+if [ -n "$DEBUG" ]; then
+  out_009=009.cons.out
+  out_016=016.cons.out
+  out_071=071.cons.out
+  log DEBUG "Transitional output files:
+  $out_009
+  $out_016
+  $out_071"
+fi
+
+# 0      | 1              | 2        | 3          | 4
+# |  5        | 6          | 7
+$cmd_009 | tee "$out_009" | $cmd_016 | eop_filter | tee "$out_016" \
+  |  $cmd_071 | eop_filter | tee "$out_071"
+
+status="${PIPESTATUS[5]}"
+
+[ $status -eq 0 ] && status_info=OK || status_info=FAIL
+log INFO "Consistency check finished, status: $status_info"
+
+exit $status

Utils/Dataflow/run/data4es-consistency-check
@@ -1,60 +1,1 @@
-#!/bin/bash -l
-
-DEBUG=
-
-base_dir=$( cd "$(dirname "$(readlink -f "$0")")"; pwd)
-lib="$base_dir/../shell_lib"
-
-# Directories with configuration files
-[ -n "$DATA4ES_CONSISTENCY_CONFIG_PATH" ] && \
-    CONFIG_PATH="$DATA4ES_CONSISTENCY_CONFIG_PATH" || \
-    CONFIG_PATH="${base_dir}/../config:${base_dir}/../../Elasticsearch/config"
-
-source $lib/get_config
-source $lib/eop_filter
-
-# Oracle
-cfg009=`get_config "consistency009.cfg"`
-cmd_009="$base_dir/../009_oracleConnector/Oracle2JSON.py --config $cfg009"
-
-# Formatting
-cmd_016="$base_dir/../016_task2es/task2es.py -m s"
-
-# ES
-cfg_es=`get_config "es"`
-cmd_071="$base_dir/../071_esConsistency/consistency.py -m s --conf $cfg_es"
-
-while [ -n "$1" ]; do
-  case $1 in
-    --debug)
-      DEBUG=1;;
-    *)
-      break;;
-  esac
-  shift
-done
-
-out_009=/dev/null
-out_016=/dev/null
-out_071=/dev/null
-if [ -n "$DEBUG" ]; then
-  out_009=009.cons.out
-  out_016=016.cons.out
-  out_071=071.cons.out
-  log DEBUG "Transitional output files:
-  $out_009
-  $out_016
-  $out_071"
-fi
-
-# 0      | 1              | 2        | 3          | 4
-# |  5        | 6          | 7
-$cmd_009 | tee "$out_009" | $cmd_016 | eop_filter | tee "$out_016" \
-  |  $cmd_071 | eop_filter | tee "$out_071"
-
-status="${PIPESTATUS[5]}"
-
-[ $status -eq 0 ] && status_info=OK || status_info=FAIL
-log INFO "Consistency check finished, status: $status_info"
-
-exit $status
+../data4es/run/data4es-consistency-check

commit 5e83d4092a2c03a8171924d4c68d693c1e0146a8

DF/*: fix path to default ES config (again).
The path was broken in 70bb8e9 and was only fixed in a couple of places
instead of all the affected files.

Utils/Dataflow/data4es-nested/019_esFormat/run.sh
@@ -2,7 +2,7 @@
 
 base_dir=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
 
-ES_CONFIG=$base_dir/../../Elasticsearch/config/es
+ES_CONFIG=$base_dir/../../../Elasticsearch/config/es
 CONFIG_DEFAULT=TRUE
 
 usage() {

Utils/Dataflow/data4es-nested/069_upload2es/load_data.sh
@@ -12,7 +12,7 @@ PARAMETERS:
 
 base_dir=$( cd "$( dirname "$( readlink -f "$0" )" )" && pwd )
 
-ES_CONFIG="${base_dir}/../../Elasticsearch/config/es"
+ES_CONFIG="${base_dir}/../../../Elasticsearch/config/es"
 
 . "$base_dir"/../shell_lib/log
 

Utils/Dataflow/data4es-nested/run/data4es-consistency-check
@@ -8,7 +8,7 @@ lib="$base_dir/../shell_lib"
 # Directories with configuration files
 [ -n "$DATA4ES_CONSISTENCY_CONFIG_PATH" ] && \
     CONFIG_PATH="$DATA4ES_CONSISTENCY_CONFIG_PATH" || \
-    CONFIG_PATH="${base_dir}/../config:${base_dir}/../../Elasticsearch/config"
+    CONFIG_PATH="${base_dir}/../config:${base_dir}/../../../Elasticsearch/config"
 
 source $lib/get_config
 source $lib/eop_filter

Utils/Dataflow/data4es/069_upload2es/load_data.sh
@@ -12,7 +12,7 @@ PARAMETERS:
 
 base_dir=$( cd "$( dirname "$( readlink -f "$0" )" )" && pwd )
 
-ES_CONFIG="${base_dir}/../../Elasticsearch/config/es"
+ES_CONFIG="${base_dir}/../../../Elasticsearch/config/es"
 
 . "$base_dir"/../shell_lib/log
 

Utils/Dataflow/data4es/run/data4es-consistency-check
@@ -8,7 +8,7 @@ lib="$base_dir/../shell_lib"
 # Directories with configuration files
 [ -n "$DATA4ES_CONSISTENCY_CONFIG_PATH" ] && \
     CONFIG_PATH="$DATA4ES_CONSISTENCY_CONFIG_PATH" || \
-    CONFIG_PATH="${base_dir}/../config:${base_dir}/../../Elasticsearch/config"
+    CONFIG_PATH="${base_dir}/../config:${base_dir}/../../../Elasticsearch/config"
 
 source $lib/get_config
 source $lib/eop_filter