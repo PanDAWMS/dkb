34b92dddeddaf9350647a66d0613601e9ef160b4
Marina Golosova <golosova.marina@gmail.com>
2020-09-17 12:43:05 +0200

    API/storages: refactoring (DKBStorageMethod).
    Replace explicit manual definitions of `storage` module functions (in
    `__init__`) with wrapper class `DKBStorageMethod` of callable objects.
    
    Each instance of this class, basically, maps call of
    `storage.func_name()` to `storage.<submodule>.func_name()`.
    
    List of method names and which submodule to use for each of them is
    defined by `methods` hash variable.

--- Utils/API/server/lib/dkb/api/storages/__init__.py
+++ Utils/API/server/lib/dkb/api/storages/__init__.py
@@ -5,105 +5,37 @@ Module responsible for interaction with DKB storages.
 import es
 
 
-def task_steps_hist(**kwargs):
-    """ Get data for histogram of task distribution over time.
+class DKBStorageMethod(object):
+    """ Wrapper class for all storage methods. """
 
-    :return: hash of data for the histogram
-    :rtype: dict
-    """
-    return es.task_steps_hist(**kwargs)
+    def __init__(self, name, storage):
+        self.name = name
+        try:
+            self.action = getattr(storage, name)
+        except AttributeError:
+            # Not defined
+            pass
 
+    def __call__(self, **kwargs):
+        return self.action(**kwargs)
 
-def task_chain(**kwargs):
-    """ Reconstruct task chain from given task ID.
-
-    :param tid: task ID
-    :type tid: int, str
-
-    :return: task chain data
-    :rtype: dict
-    """
-    return es.task_chain(**kwargs)
-
-
-def task_kwsearch(**kwargs):
-    """ Search tasks and related datasets by keywords.
-
-    .. note: For wildcard keywords only the ``taskname`` field is used.
-
-    :param kw: list of (string) keywords
-    :type kw: list
-    :param analysis: if analysis tasks should be searched
-    :type analysis: str, bool
-    :param production: if production tasks should be searched
-    :type production: str, bool
-    :param size: number of documents in response
-    :type size: str, int
-    :param ds_size: max number of datasets returned for each task
-    :type ds_size: str, int
-    :param timeout: request execution timeout (sec)
-    :type timeout: str, int
-
-    :return: task and related datasets metadata
-    :rtype: dict
-    """
-    return es.task_kwsearch(**kwargs)
+    def action(self, **kwargs):
+        """ Method action placeholder. """
+        raise NotImplementedError("Method '%(name)s' is not implemented yet."
+                                  % {'name': self.name})
 
 
-def task_derivation_statistics(**kwargs):
-    """ Calculate statistics of derivation efficiency.
-
-    :param project: project name
-    :type project: str
-    :param amitag: amitag (or several)
-    :type amitag: str or list
-
-    :return: calculated statistics in format required by
-             :py:func:`api.handlers.task_deriv`
-    :rtype: dict
-    """
-    return es.task_derivation_statistics(**kwargs)
+# Hash of public method names and storage submodules in which to look for them
+methods = {
+    'task_steps_hist': es,
+    'task_chain': es,
+    'task_kwsearch': es,
+    'task_derivation_statistics': es,
+    'campaign_stat': es,
+    'step_stat': es
+}
 
 
-def campaign_stat(**kwargs):
-    """ Calculate values for campaign progress overview.
-
-    :param selection_params: hash of parameters defining task selections
-                             (for details see
-                              :py:func:`es.common.get_selection_query`)
-    :type selection_params: dict
-    :param step_type: step definition type: 'step', 'ctag_format'
-                      (default: 'step')
-    :type step_type: str
-
-    :param events_src: source of data for 'output' events.
-                       Possible values:
-                       * 'ds'   -- number of events in output datasets;
-                       * 'task' -- number of processed events of 'done'
-                                   and 'finished' tasks;
-                       * 'all'  -- provide all possible values as hash.
-    :type events_src: str
-
-    :return: hash with calculated statistics for ``campaign/stat`` method
-             (see :py:func:`api.handlers.campaign_stat`)
-    :rtype: dict
-    """
-    return es.campaign_stat(**kwargs)
-
-
-def step_stat(**kwargs):
-    """ Calculate statistics for tasks by execution steps.
-
-    :param selection_params: hash of parameters defining task selections
-                             (for details see
-                              :py:func:`es.common.get_selection_query`)
-    :type selection_params: dict
-    :param step_type: step definition type: 'step', 'ctag_format'
-                      (default: 'step')
-    :type step_type: str
-
-    :return: hash with calculated statistics for ``step/stat`` method
-             (see :py:func:`api.handlers.step_stat`)
-    :rtype: hash
-    """
-    return es.step_stat(**kwargs)
+# Define callable objects for public methods
+for m in methods:
+    globals()[m] = DKBStorageMethod(m, methods[m])
