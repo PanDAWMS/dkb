973609a43905a2d53f2e6c41beba52dd4a8fef5e
Marina Golosova <golosova.marina@gmail.com>
2020-10-28 13:00:26 +0100

    ES/nested/reindex: extend in-between transformations.
    Changes:
    * list value of dataset field 'data_format' is replaced with the list's
      first value;
    * task's 'conditions_tags' is renamed to 'confitions_tag';
    * task's 'ctag_format_step' field is added;
    * task's 'output' field is removed.
    
    ToDo:
    * add 'ami_tags', 'ami_tags_format_step';
    * remove 'jobConfig', 'evgenJobOpts' (?);
    * ...?

--- Utils/Elasticsearch/tools/reindex-to-nested.sh
+++ Utils/Elasticsearch/tools/reindex-to-nested.sh
@@ -84,9 +84,16 @@ transform_to_nested() {
                      \"_id\": ._id}
                   },
                   {\"output_dataset\":
-                    .inner_hits.datasets.hits.hits 
-                    | map(._source | .name = .datasetname | del(.datasetname))
-                  } + ._source" \
+                    .inner_hits.datasets.hits.hits
+                    | map(._source | .name = .datasetname
+                          | .data_format = .data_format[0]
+                          | del(.datasetname)),
+                   \"conditions_tag\": ._source.conditions_tags,
+                   \"ctag_format_step\":
+                    (._source as \$src | \$src.output_formats
+                     | map( . + \":\" + \$src.ctag))
+                  } + ._source | del(.conditions_tags)
+                    | del(.output)" \
   && echo ''
 }
 
