4c1d6a2eeb869a891733965909e655b2fc23aaf8
Marina Golosova <golosova.marina@gmail.com>
2021-04-15 12:29:47 +0200

    feat(ES/tools): allow specifying path to ES endpoint
    E.g. if ES entry point has address like 'HOST:PORT/es' (as in case of
    the the CERN ES Service instance), it was impossible to specify just as
    '${ES_HOST}:${ES_PORT}' combination.
    
    Now it can be extended with 'ES_PATH' parameter.

--- Utils/Elasticsearch/tools/load_mapping.sh
+++ Utils/Elasticsearch/tools/load_mapping.sh
@@ -10,6 +10,7 @@ EOF
 ES_CONFIG="$(dirname $0)/../config/es"
 ES_HOST='127.0.0.1'
 ES_PORT='9200'
+ES_PATH=''
 
 while getopts c: opt; do
   case $opt in		
@@ -21,8 +22,10 @@ done
 [ -n "$ES_USER" -a "$ES_PASSWORD" ] && ES_AUTH="--user ${ES_USER}:${ES_PASSWORD}"
 shift $((OPTIND-1)) 
 
+[ "$ES_PATH" == '/' ] && ES_PATH=''
+
 [ -z "$1" -o ! -f "${1}" ] \
   && echo "You must provide path to mapping file!" \
   || curl $ES_AUTH -H "Content-Type: application/x-ndjson" \
           -XPUT --data-binary "@${1}" \
-          "http://${ES_HOST}:${ES_PORT}/_template/`basename ${1%.*}`?pretty"
+          "http://${ES_HOST}:${ES_PORT}${ES_PATH}/_template/`basename ${1%.*}`?pretty"
