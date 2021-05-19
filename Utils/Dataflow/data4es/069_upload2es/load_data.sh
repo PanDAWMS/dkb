fa1d23474379f6606aac5a2a1dcfc5c57e8e57a8
Marina Golosova <golosova.marina@gmail.com>
2021-05-19 13:11:14 +0200

    feat(DF/069): use ES access protocol configuration parameter
    Previously it was hard-coded as 'http'.

--- Utils/Dataflow/data4es/069_upload2es/load_data.sh
+++ Utils/Dataflow/data4es/069_upload2es/load_data.sh
@@ -74,6 +74,7 @@ done
 log "Loading defaults and config $ES_CONFIG if any"
 ES_HOST='127.0.0.1'
 ES_PORT='9200'
+ES_PROTO='http'
 
 [ -f "$ES_CONFIG" ] && source "$ES_CONFIG"
 [ -n "$ES_USER" -a "$ES_PASSWORD" ] && ES_AUTH="--user ${ES_USER}:${ES_PASSWORD}"
@@ -83,7 +84,7 @@ SLEEP=5
 DELIMITER=`echo -e -n "\x00"`
 EOProcess=`echo -e -n "\x06"`
 
-cmd="curl -sS $ES_AUTH http://$ES_HOST:$ES_PORT/_bulk?pretty --data-binary @"
+cmd="curl -sS $ES_AUTH $ES_PROTO://$ES_HOST:$ES_PORT/_bulk?pretty --data-binary @"
 
 load_files () {
   [ -z "$1" -o ! -f "$1" ] && log $(usage) && exit 1
